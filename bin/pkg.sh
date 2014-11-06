8583e4bfb6db7437416d163cc5ca888dfe4b4ab7
Oleg Kolosov <bazurbat@gmail.com>
2014-11-06 16:14:32 +0400

    Added error messages to pkg script

--- bin/pkg.sh
+++ bin/pkg.sh
@@ -4,8 +4,8 @@ if [ -z "$ja_root" ]; then
     export ja_root="$(realpath $(dirname $0)/..)"
 fi
 
-. "$ja_root/lib/env.sh" || exit
-. "$ja_root/lib/pkg.sh" || exit
+. "$ja_root/lib/env.sh" || { echo "Failed to load env"; exit 1; }
+. "$ja_root/lib/pkg.sh" || die "Failed to load pkg env"
 
 . "$ja_lib_dir/env/cmake.sh" || exit
 
@@ -20,9 +20,9 @@ p_config="$3"
 p_log="${ja_build_dir}/${p_name}-${p_stage}${p_config:+-${p_config}}.log"
 p_work_dir="$p_build_root/$p_name"
 
-rm -f "$p_log" || exit
-mkdir -p "$p_work_dir" || exit
-cd "$p_work_dir" || exit
+rm -f "$p_log" || die "Failed to rm $p_log"
+mkdir -p "$p_work_dir" || die "Failed to mkdir $p_work_dir"
+cd "$p_work_dir" || die "Failed to cd $p_work_dir"
 
 include "$ja_lib_dir/pkg/$p_name" 
 
@@ -30,8 +30,8 @@ p_source_name=$(basename "$p_source" | sed -r 's/\.t(ar\.)?(gz|bz2|xz)//')
 p_source_dir="${p_source_dir:-$p_work_dir/$p_source_name}"
 p_build_dir="${p_build_dir:-$p_source_dir}"
 
-mkdir -p "$p_build_dir" || exit
-cd "$p_build_dir" || exit
+mkdir -p "$p_build_dir" || die "Failed to mkdir $p_build_dir"
+cd "$p_build_dir" || die "Failed to cd $p_build_dir"
 
 p_stage_function="pkg_${p_stage}"
 if [ "$p_config" ]; then
