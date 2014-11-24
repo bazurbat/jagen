2ab9f41f961d047d4062825739192235fc92b7bf
Oleg Kolosov <bazurbat@gmail.com>
2014-11-24 11:55:32 +0300

    Updated ez rules

--- lib/rules.ez.scm
+++ lib/rules.ez.scm
@@ -1,103 +1,88 @@
-(pkg 'gdb
-     '((clean)
-       (unpack)
-       (prepare)
-       (config host
-               (build)
-               (install))))
-
-(pkg 'rootfs
-     '((unpack)
-       (prepare)))
+(pkg 'rootfs)
 
 (pkg 'u-boot
-     '((clean)
-       (unpack)
-       (config min
-               (build (rootfs prepare)))
-       (config target
-               (build (u-boot build min))
-               (mkimage))))
+     '(config min
+              (build (rootfs prepare)))
+     '(config target
+              (build (u-boot build min))
+              (mkimage)))
 
 (pkg 'linux
-     '((clean)
-       (unpack)
-       (config target
-               (build)
-               (install (rootfs prepare))
-               (depmod (cmem install target)
-                       (syslink install)))))
+     '(config target
+              (build)
+              (install (rootfs prepare))
+              (depmod (cmem install target)
+                      (syslink install))))
 
 (pkg 'cmem
-     '((config target
-               (unpack (rootfs prepare))
-               (build (linux install target))
-               (install))))
+     '(config target
+              (unpack (rootfs prepare))
+              (build (linux install target))
+              (install)))
 
 (pkg 'syslink
-     '((clean)
-       (unpack (rootfs prepare))
-       (build (linux install target))
-       (install)))
+     '(unpack (rootfs prepare))
+     '(build (linux install target))
+     '(install))
 
 (if #f
   (begin
 
+(pkg 'gdb
+     '(config host
+              (build)
+              (install)))
+
 (pkg 'chibi-scheme
-     '((config target
-               (unpack)
-               (build (chibi-scheme install host))
-               (install))))
+     '(config target
+              (unpack)
+              (build (chibi-scheme install host))
+              (install)))
 
 (pkg 'chicken
-     '((unpack)
-       (prepare)
-       (config host
-               (build)
-               (install))
-       (config cross
-               (build after (chicken install host))
-               (install))
-       (config target
-               (build after (chicken install cross))
-               (install (rootfs prepare)))))
+     '(config host
+              (build)
+              (install))
+     '(config cross
+              (build after (chicken install host))
+              (install))
+     '(config target
+              (build after (chicken install cross))
+              (install (rootfs prepare))))
 
 (pkg 'libuv
-     '((config host
-               (unpack)
-               (prepare)
-               (build)
-               (install))
-       (config target
-               (unpack)
-               (prepare)
-               (build (libuv install host))
-               (install (rootfs prepare)))))
+     '(config host
+              (unpack)
+              (prepare)
+              (build)
+              (install))
+     '(config target
+              (unpack)
+              (prepare)
+              (build (libuv install host))
+              (install (rootfs prepare))))
 
 (pkg 'luajit
-     '((config host
-               (unpack)
-               (build)
-               (install))
-       (config target
-               (unpack)
-               (build (luajit install host))
-               (install (rootfs prepare)))))
+     '(config host
+              (unpack)
+              (build)
+              (install))
+     '(config target
+              (unpack)
+              (build (luajit install host))
+              (install (rootfs prepare))))
 
 (pkg 'ffmpeg
-     '((unpack)
-       (config host
-               (build)
-               (install))
-       (config target
-               (build)
-               (install (rootfs prepare)))))
+     '(config host
+              (build)
+              (install))
+     '(config target
+              (build)
+              (install (rootfs prepare))))
 
 (pkg 'soundtouch
-     '((unpack)
-       (prepare)
-       (config target
-               (build)
-               (install (rootfs prepare)))))
+     '(config target
+              (build)
+              (install (rootfs prepare))))
 
 ))
