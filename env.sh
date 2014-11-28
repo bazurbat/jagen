74747389ed86b15ab7b23b1e89c5cb767993af24
Oleg Kolosov <bazurbat@gmail.com>
2014-11-28 15:16:26 +0300

    Removed refresh from global env

--- env.sh
+++ env.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 
-run_refresh="yes"
-[ "$1" = "--no-refresh" ] && run_refresh="no"
-
 export pkg_root=$(realpath .)
 
 . "$pkg_root/lib/env.sh" ||
     { echo "Failed to load environment"; exit 1; }
 
 PATH="$pkg_root/bin:$pkg_private_dir/bin:$PATH"
-
-[ "$run_refresh" = "yes" ] && jagen refresh
