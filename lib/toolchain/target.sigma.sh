2dd910b5e5fe8362d5a664b3842b323434e7bdf5
Oleg Kolosov <bazurbat@gmail.com>
2014-12-01 15:49:49 +0300

    Added function to adjust cflags for sigma target

--- lib/toolchain/target.sigma.sh
+++ lib/toolchain/target.sigma.sh
@@ -13,3 +13,22 @@ export LDFLAGS=""
 export PKG_CONFIG_SYSROOT_DIR="$sdk_rootfs_prefix"
 export PKG_CONFIG_LIBDIR="${PKG_CONFIG_SYSROOT_DIR}/lib/pkgconfig"
 export PKG_CONFIG_PATH="$target_dir$target_prefix/lib/pkgconfig"
+
+gcc_version() { "$1" --version | awk "/gcc/ { print \$NF; }"; }
+
+adjust_cflags() {
+    local IFS version=$(gcc_version "$CC")
+    IFS="."
+    set -- $version
+
+    [ $1 != 4 ] && die "Unsupported GCC version: $version"
+
+    if [ $2 -ge 6 ]; then
+        CFLAGS="$CFLAGS -fno-strict-aliasing"
+        CFLAGS="$CFLAGS -Wno-unused-but-set-variable"
+    fi
+
+    CXXFLAGS="$CFLAGS"
+}
+
+adjust_cflags
