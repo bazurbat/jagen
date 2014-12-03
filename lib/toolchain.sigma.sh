15e9cef662e6de6ce50009977d0e9a4f0aa6bae5
Oleg Kolosov <bazurbat@gmail.com>
2014-12-03 15:41:04 +0300

    Fixed flags in generated toolchain

--- lib/toolchain.sigma.sh
+++ lib/toolchain.sigma.sh
@@ -3,17 +3,16 @@
 common_tools="addr2line ar c++filt elfedit gcov gdb gdbtui gprof nm objcopy
 objdump ranlib readelf size sprite strings strip"
 
-gen_opt="-EL"
 inc_opt="-isystem \"$sdk_rootfs_prefix/include\""
 lib_opt="-L\"$sdk_rootfs_prefix/lib\""
 
 generate_toolchain_wrappers() {
 	mkdir -p "$target_bin_dir"
 
-	generate_tool ld "$gen_opt"
-	generate_tool as "$gen_opt $inc_opt"
+	generate_tool ld -EL
+	generate_tool as -EL "$inc_opt"
 	for name in c++ cpp g++ gcc; do
-		generate_tool $name "$gen_opt $inc_opt $lib_opt"
+		generate_tool $name -EL "$inc_opt $lib_opt"
 	done
 	for name in $common_tools; do
 		generate_tool $name
@@ -22,9 +21,9 @@ generate_toolchain_wrappers() {
 }
 
 generate_tool() {
-	local name="$1" options="$2"
+	local name="$1" pre_opt="$2" post_opt="$3"
 	cat <<EOF >"${target_bin_dir}/${target_system}-${name}"
 #!/bin/sh
-exec mips-linux-gnu-${name} "\$@" $options
+exec mips-linux-gnu-${name} $pre_opt "\$@" $post_opt
 EOF
 }
--- lib/toolchain/target.sigma.sh
+++ lib/toolchain/target.sigma.sh
@@ -6,7 +6,7 @@ export CC="${target_system}-gcc"
 export CXX="${target_system}-g++"
 export STRIP="${target_system}-strip"
 
-export CFLAGS="-O2 -fomit-frame-pointer -fno-strict-aliasing
+export CFLAGS="-O2 -fomit-frame-pointer -fno-strict-aliasing \
 -Wa,-mips32r2 -march=24kf -mtune=24kf -pipe"
 export CXXFLAGS="$CFLAGS"
 export LDFLAGS=""
