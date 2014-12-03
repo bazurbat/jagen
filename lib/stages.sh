501a092d983e2c4fea7334134a8899d4911da1b1
Oleg Kolosov <bazurbat@gmail.com>
2014-12-03 10:53:23 +0300

    Added offline p_flag support

--- lib/stages.sh
+++ lib/stages.sh
@@ -25,7 +25,9 @@ pkg_unpack() {
 
     case $kind in
         git|hg)
-            if p_in_list "$p_name" "$pkg_source_exclude"; then
+            if p_flags "offline"; then
+                message "Offline mode, not checking $p_name"
+            elif p_in_list "$p_name" "$pkg_source_exclude"; then
                 message "pkg source '$p_name' excluded from pulling"
             elif [ -d "$p_source_dir" ]; then
                 if p_src_is_dirty "$p_source_dir"; then
