c1af6b42bc34a8ffff19e00df7621c51e446c029
Oleg Kolosov <bazurbat@gmail.com>
2014-12-05 14:39:02 +0300

    Fixed separator handling in list_remove

--- lib/list.sh
+++ lib/list.sh
@@ -3,11 +3,15 @@
 p_in_list() { echo "$2" | grep -qw "$1"; }
 
 p_list_remove() {
-    local list=$1 item=$2 s=${3:-:}
-    echo "$list" | sed -e "s|${s}\?${item}${s}\?||g"
+    local list item S
+    list="$1" item="$2" S=${3:-:}
+
+    echo "$list" |
+    sed "s|\(.*${S}\?\)${item}\(.*${S}\?\)|\1\2|g" |
+    sed "s|$S$S|$S|g"
 }
 
 p_path_prepend() {
     local item="$1"
-    PATH=${item}:$(p_list_remove "$PATH" "$item" :)
+    PATH=${item}:$(p_list_remove "$PATH" "$item")
 }
