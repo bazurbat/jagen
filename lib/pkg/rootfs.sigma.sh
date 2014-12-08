650edd516d9178432ada2e7981906598e6b24e4d
Oleg Kolosov <bazurbat@gmail.com>
2014-12-08 16:43:33 +0300

    Added status message to rootfs install

--- lib/pkg/rootfs.sigma.sh
+++ lib/pkg/rootfs.sigma.sh
@@ -66,38 +66,53 @@ pkg_install() {
 
     p_run cd "$sdk_rootfs_root"
 
+    p_run echo "Cleaning directories"
     p_run rm -fr dev opt proc sys root tmp usr
-    p_run rm -f init linuxrc
     p_run install -m 700 -d root
+
+    p_run echo "Fixing init link"
+    p_run rm -f init linuxrc
     p_run ln -s /bin/busybox init
 
     p_run cd "$sdk_rootfs_root/bin"
 
+    p_run echo "Cleaning bin"
     p_run rm -f mtd_* *.bash
     p_run rm -f setxenv unsetxenv
+    p_run echo "Setting setxenv2 links"
     p_run ln -fs setxenv2_mipsel setxenv2
     p_run ln -fs setxenv2_mipsel unsetxenv2
 
     p_run cd "$sdk_rootfs_root/etc"
 
+    p_run echo "Cleaning etc"
     p_run rm -fr init.d network cs_rootfs_*
     p_run rm -f inputrc ld.so.cache mtab
 
+    p_run echo "Creating network directories"
     for d in up down pre-up post-down; do
         p_run mkdir -p network/if-${d}.d
     done
 
     p_run cd "$sdk_rootfs_root/lib"
-    p_run rm -f libnss_compat* libnss_hesiod* libnss_nis*
 
+    p_run echo "Cleaning lib"
+    p_run rm -f libnss_compat* libnss_hesiod* libnss_nis*
     find "$sdk_rootfs_root/lib" \( -name "*.a" -o -name "*.la" \) -delete
 
+    p_run echo "Installing timezone"
     install_timezone
+    p_run echo "Installing keys"
     install_keys
+    p_run echo "Installing gpg"
     install_gpg
+    p_run echo "Installing losetup"
     install_losetup
+    p_run echo "Installing ldconfig"
     install_ldconfig
+    p_run echo "Installing files"
     install_files
 
+    p_run echo "Stripping"
     p_strip "$sdk_rootfs_root"
 }
