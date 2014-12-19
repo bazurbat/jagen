0c030faa617ae32a8387fed3e5ea265f52d8e42e
Oleg Kolosov <bazurbat@gmail.com>
2014-12-19 17:14:26 +0300

    Enabled more wpa_supplicant options

--- lib/pkg/wpa_supplicant.sigma.sh
+++ lib/pkg/wpa_supplicant.sigma.sh
@@ -20,25 +20,36 @@ pkg_build() {
 
     echo "BINDIR=$p_prefix/bin"              >> .config
 
-    echo "CONFIG_CTRL_IFACE=y"               >> .config
-    echo "CONFIG_BACKEND=file"               >> .config
+    echo "CONFIG_NO_STDOUT_DEBUG=y"          >> .config
 
-    echo "CONFIG_PEERKEY=y"                  >> .config
+    echo "CONFIG_BACKEND=file"               >> .config
+    echo "CONFIG_CTRL_IFACE=y"               >> .config
+    echo "CONFIG_CTRL_IFACE_DBUS_NEW=y"      >> .config
+    echo "CONFIG_CTRL_IFACE_DBUS_INTRO=y"    >> .config
+
+    echo "CONFIG_IBSS_RSN=y"                 >> .config
+    echo "CONFIG_IEEE80211W=y"               >> .config
+    echo "CONFIG_IEEE80211R=y"               >> .config
+    echo "CONFIG_IEEE80211N=y"               >> .config
 
     echo "CONFIG_BGSCAN_SIMPLE=y"            >> .config
     echo "CONFIG_BGSCAN_LEARN=y"             >> .config
+    echo "CONFIG_AUTOSCAN_EXPONENTIAL=y"     >> .config
+    echo "CONFIG_AUTOSCAN_PERIODIC=y"        >> .config
+
+    echo "CONFIG_DRIVER_WEXT=y"              >> .config
+    # echo "CONFIG_DRIVER_NL80211=y"           >> .config
 
     echo "CONFIG_TLS=internal"               >> .config
     echo "CONFIG_INTERNAL_LIBTOMMATH=y"      >> .config
     echo "CONFIG_INTERNAL_LIBTOMMATH_FAST=y" >> .config
 
-    echo "CONFIG_CTRL_IFACE_DBUS_NEW=y"      >> .config
-
-    echo "CONFIG_DRIVER_WEXT=y"              >> .config
-
+    echo "CONFIG_PEERKEY=y"                  >> .config
+    echo "CONFIG_IEEE8021X_EAPOL=y"          >> .config
     echo "CONFIG_DELAYED_MIC_ERROR_REPORT=y" >> .config
 
-    echo "CONFIG_IEEE8021X_EAPOL=y"          >> .config
+    echo "CONFIG_ELOOP=eloop"                >> .config
+    echo "CONFIG_ELOOP_EPOLL=y"              >> .config
 
     p_run make
 }
