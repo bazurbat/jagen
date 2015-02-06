41e71e05eb53624b316399415362fec1a9284fd3
Oleg Kolosov <bazurbat@gmail.com>
2015-02-06 18:39:35 +0300

    Changed default new kernel release

--- lib/env/sdk.sigma.sh
+++ lib/env/sdk.sigma.sh
@@ -51,7 +51,14 @@ fi
 export COMPILKIND
 
 if in_flags "new_kernel"; then
-    kernel_release="3.18.3-sigma+"
+    # kernel_release="2.6.32.15-sigma"
+    # kernel_release="3.0.101-sigma+"
+    kernel_release="3.4.105-sigma+"
+    # kernel_release="3.5.0-sigma+"
+    # kernel_release="3.6.11-sigma+"
+    # kernel_release="3.7.10-sigma+"
+    # kernel_release="3.10.65-sigma+"
+    # kernel_release="3.18.3-sigma+"
 else
     kernel_release="2.6.32.15-21-sigma"
 fi
