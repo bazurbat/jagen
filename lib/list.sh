32f3e0c5bf7e6b0378b9701e451571cc3709e86d
Oleg Kolosov <bazurbat@gmail.com>
2015-02-10 12:02:47 +0300

    core: improve shell list handling

--- lib/list.sh
+++ lib/list.sh
@@ -7,11 +7,18 @@ p_list_remove() {
     list="$1" item="$2" S=${3:-:}
 
     echo "$list" |
-    sed "s|\(.*${S}\?\)${item}\(.*${S}\?\)|\1\2|g" |
-    sed "s|$S$S|$S|g"
+    sed "s|\(.*\)${S}\?${item}${S}\?\(.*\)|\1\2|g" |
+    sed "s|$S$S|$S|g" |
+    sed "s|^:\(.*\)|\1|" |
+    sed "s|\(.*\):$|\1|"
 }
 
 p_path_prepend() {
     local item="$1"
-    PATH=${item}:$(p_list_remove "$PATH" "$item")
+    PATH="$item":$(p_list_remove "$PATH" "$item")
+}
+
+p_ld_library_path_prepend() {
+    local item="$1"
+    LD_LIBRARY_PATH="$item":$(p_list_remove "$LD_LIBRARY_PATH" "$item")
 }
