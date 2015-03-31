2c56e5b45ad4d32bf57c69ace1391b1c34046759
Oleg Kolosov <bazurbat@gmail.com>
2015-03-31 13:10:18 +0300

    sigma: support different ucode packages

--- lib/pkg/ucode.sigma.sh
+++ lib/pkg/ucode.sigma.sh
@@ -2,14 +2,20 @@
 
 pkg_install() {
     local dst="$sdk_firmware_dir/xload"
+    local dts real
 
     p_run install -vd "$dst"
 
+    case $pkg_sdk_version in
+        308) dts="_nodts" ;;
+        *)   dts="_0x10_nodts"; real="_noreal" ;;
+    esac
+
     p_run install -vm 644 \
-        "audio_microcode_t3iptv_prod_0x10_nodts.xload" \
+        "audio_microcode_t3iptv_prod${dts}.xload" \
         "$dst/audio_microcode_tango3.xload"
     p_run install -vm 644 \
-        "video_microcode_t3iptv_prod_noreal.xload" \
+        "video_microcode_t3iptv_prod${real}.xload" \
         "$dst/video_microcode_tango3.xload"
     p_run install -vm 644 \
         "demuxpsf_microcode_t3iptv_prod.xload" \
