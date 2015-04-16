25e1d35a0d0460fcc81efed69078af2d184b0323
Oleg Kolosov <bazurbat@gmail.com>
2015-04-16 18:12:17 +0300

    hisi: more error handling to generate toolchan wrappers

--- lib/toolchain.hisilicon.sh
+++ lib/toolchain.hisilicon.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-toolchain_bin=$(dirname $(command -v arm-hisiv200-linux-gnueabi-gcc 2>/dev/null))
-
 generate_toolchain_wrappers() {
-    local name
+    local bin name
+    local gcc_file gcc_dir
+    local gcc_name=arm-hisiv200-linux-gnueabi-gcc
+
+    gcc_file="$(command -v "$gcc_name" 2>/dev/null)"
+    [ "$gcc_file" ] ||
+        die "Toolchain executable '$gcc_name' was not found in path"
+    gcc_dir="$(dirname "$gcc_file")"
 
     [ -d "$target_bin_dir" ] && rm -r "$target_bin_dir"
     mkdir -p "$target_bin_dir"
 
-    for bin in "$toolchain_bin"/*; do
-        name=$(basename $bin | cut -d- -f5-)
+    for bin in "$gcc_dir"/*; do
+        name="$(basename "$bin" | cut -d- -f5-)"
         ln -s "$bin" "$target_bin_dir/${target_system}-${name}"
     done
 }
