05fa3df866953234db634ba69edcc7117cbef245
Oleg Kolosov <bazurbat@gmail.com>
2015-04-16 15:15:01 +0300

    hisi: adjust rules and sdk env

--- lib/env/sdk.hisilicon.sh
+++ lib/env/sdk.hisilicon.sh
@@ -6,3 +6,9 @@ export target_arch="arm"
 export target_system="arm-hisiv200-linux"
 
 export target_bin_dir="$target_dir/bin"
+
+export JAVA_HOME="/usr/lib/jvm/sun-jdk-1.6"
+export JAVAC="$JAVA_HOME/bin/javac"
+# export _JAVA_OPTIONS="-Xms1024m -Xmx2048m -XX:MaxPermSize=256m"
+
+p_path_prepend "$JAVA_HOME/bin"
--- lib/rules.hisilicon.scm
+++ lib/rules.hisilicon.scm
@@ -6,26 +6,7 @@
          (stage 'patch)
          args))
 
-(define (rootfs-package name . args)
-  (apply package name
-         (stage 'build (depends (target 'rootfs 'build)))
-         (stage 'install)
-         args))
-
-(define (kernel-package name . args)
-  (apply package name
-         (stage 'build (depends (target 'kernel 'build)))
-         (stage 'install)
-         args))
-
-(define (firmware-package name . args)
-  (apply package name
-         (stage 'build)
-         (stage 'install (depends (target 'firmware 'unpack)))
-         args))
-
 (package 'make
-  (source 'dist "make-3.81.tar.bz2")
+  (source 'dist "make-3.82.tar.bz2")
   (stage 'host 'build)
   (stage 'host 'install))
-
