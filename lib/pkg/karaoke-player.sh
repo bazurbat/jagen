f43db9c8ee1b45cce62ff2133ee4fb4bc3c56c9a
Oleg Kolosov <bazurbat@gmail.com>
2015-05-21 19:04:30 +0300

    hisilicon: add freetype and karaoke-player

---
+++ lib/pkg/karaoke-player.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+p_source_dir="$pkg_src_dir/karaoke-player"
+p_build_dir="$p_work_dir/build${p_config:+-$p_config}"
+
+pkg_build_host() {
+    in_flags chicken_next && use_env tools
+
+    p_run cmake -G"$cmake_generator" \
+        -DCMAKE_BUILD_TYPE="$cmake_build_type" \
+        -DCMAKE_INSTALL_PREFIX="$host_dir" \
+        -DCMAKE_FIND_ROOT_PATH="$host_dir" \
+        "$p_source_dir"
+
+    p_run cmake --build . -- $cmake_build_options
+}
+
+pkg_build_target() {
+    use_env host
+
+    case $sdk_target_board in
+        ast50|ast100)
+            p_run cmake -G"$cmake_generator" \
+                -DCMAKE_BUILD_TYPE="$cmake_build_type" \
+                -DCMAKE_SYSTEM_NAME="Linux" \
+                -DCMAKE_INSTALL_PREFIX="${target_dir}${target_prefix}" \
+                -DCMAKE_FIND_ROOT_PATH="${target_dir}${target_prefix}" \
+                -DSIGMA_SDK_DIR="$pkg_src_dir/sigma-mrua" \
+                -DSIGMA_SDK_VERSION="$pkg_sdk_version" \
+                -DSIGMA_ROOTFS_DIR="$pkg_src_dir/sigma-rootfs" \
+                -DCHICKEN_COMPILER="$host_dir/bin/chicken" \
+                -DCHICKEN_INTERPRETER="$host_dir/bin/csi" \
+                -DCHICKEN_DEPENDS="$host_dir/bin/chicken-depends" \
+                "$p_source_dir"
+            ;;
+        *)
+            p_run cmake -G"$cmake_generator" \
+                -DCMAKE_BUILD_TYPE="$cmake_build_type" \
+                -DCMAKE_SYSTEM_NAME="Linux" \
+                -DCMAKE_INSTALL_PREFIX="${target_dir}${target_prefix}" \
+                -DCMAKE_FIND_ROOT_PATH="${target_dir}${target_prefix}" \
+                "$p_source_dir"
+            ;;
+    esac
+
+    p_run cmake --build . -- $cmake_build_options
+}
+
+pkg_install_host() {
+    p_run cmake --build . --target install
+}
+
+pkg_install_target() {
+    p_run cmake --build . --target install
+}
--- lib/rules.hisilicon.scm
+++ lib/rules.hisilicon.scm
@@ -6,6 +6,12 @@
          (stage 'patch)
          args))
 
+(define (firmware-package name . args)
+  (apply package name
+         (stage 'build)
+         (stage 'install (depends (target 'firmware 'unpack)))
+         args))
+
 (package 'ast-files
   (source 'git "git@bitbucket.org:art-system/files.git"))
 
@@ -19,8 +25,34 @@
   (stage 'target 'build   (depends (target 'ast-files 'unpack)))
   (stage 'target 'install (depends (target 'firmware  'unpack))))
 
+(firmware-package 'freetype
+  (source 'dist "freetype-2.5.0.1.tar.bz2")
+  (patch "freetype-2.3.2-enable-valid" 1)
+  (patch "freetype-2.4.11-sizeof-types" 1)
+  (patch "freetype-2.4.12-clean-include" 1))
+
+(package 'libuv
+  (source 'dist "libuv-1.4.2.tar.gz")
+  (stage 'target 'build)
+  (stage 'target 'install (depends (target 'firmware 'unpack))))
+
+(package 'astindex
+  (source 'hg "ssh://hg@bitbucket.org/art-system/astindex"
+          (directory "karaoke-player/source/astindex"))
+  (stage 'unpack (depends (target 'karaoke-player 'unpack))))
+
+(package 'karaoke-player
+  (source 'hg "ssh://hg@bitbucket.org/art-system/karaoke-player")
+  (stage 'target 'build (depends (target 'astindex     'unpack)
+                                 (target 'ffmpeg       'install 'target)
+                                 (target 'freetype     'install)
+                                 (target 'libuv        'install 'target)))
+  (stage 'target 'install))
+
 (package 'firmware
-  (stage 'install (depends (target 'ffmpeg 'install 'target)))
+  (stage 'install (depends (target 'ffmpeg 'install 'target)
+                           (target 'freetype        'install)
+                           (target 'karaoke-player  'install 'target)))
   (stage 'strip))
 
 ; vim: lw+=package,rootfs-package,kernel-package,firmware-package
