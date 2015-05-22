30324cd4429c73cfddd5c2cb27cc903602724615
Oleg Kolosov <bazurbat@gmail.com>
2015-05-22 20:24:36 +0300

    pkg: try to build libuv with gyp for android

--- lib/pkg/libuv.sh
+++ lib/pkg/libuv.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-p_build_dir="$p_work_dir/build${p_config:+-$p_config}"
+if [ "$pkg_sdk" = sigma ]; then
+    p_build_dir="$p_work_dir/build${p_config:+-$p_config}"
+fi
 
 pkg_patch() {
-    p_run cd "$p_source_dir"
-    p_run ./autogen.sh
+    if [ "$pkg_sdk" = sigma ]; then
+        p_run cd "$p_source_dir"
+        p_run ./autogen.sh
+    else
+        git clone https://chromium.googlesource.com/external/gyp.git build/gyp
+    fi
 }
 
 pkg_build_host() {
@@ -14,20 +20,37 @@ pkg_build_host() {
     p_run make
 }
 
-pkg_build_target() {
-    p_run "$p_source_dir/configure" \
-        --host="$target_system" \
-        --prefix="$target_prefix"
-
-    p_run make
-}
-
 pkg_install_host() {
     p_run make DESTDIR="$host_dir" install
     p_fix_la "$host_dir$host_prefix/lib/libuv.la" "$host_dir"
 }
 
+pkg_build_target() {
+    if [ "$pkg_sdk" = "sigma" ]; then
+        p_run "$p_source_dir/configure" \
+            --host="$target_system" \
+            --prefix="$target_prefix"
+        p_run make
+    elif [ "$pkg_sdk" = hisilicon ]; then
+        export AR=arm-linux-androideabi-ar
+        export CC=arm-linux-androideabi-gcc
+        export CXX=arm-linux-androideabi-g++
+        export LINK=arm-linux-androideabi-g++
+        export PLATFORM=android
+
+        "$p_source_dir/gyp_uv.py" -Dtarget_arch=${target_arch} -DOS=android
+        BUILDTYPE="$pkg_build_type" p_run make -C "$p_source_dir/out"
+    fi
+}
+
 pkg_install_target() {
-    p_run make DESTDIR="$target_dir" install
-    p_fix_la "$target_dir$target_prefix/lib/libuv.la" "$target_dir"
+    out_dir="$p_source_dir/out"
+
+    if [ "$pkg_sdk" = sigma ]; then
+        p_run make DESTDIR="$target_dir" install
+        p_fix_la "$target_dir$target_prefix/lib/libuv.la" "$target_dir"
+    else
+        p_run install -vm 644 "$out_dir/$pkg_build_type/libuv.a" \
+            "${target_dir}${target_prefix}/lib"
+    fi
 }
