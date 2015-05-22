800bfa96fa6dc666b50cb6571d3968786a07e143
Oleg Kolosov <bazurbat@gmail.com>
2015-05-22 20:23:03 +0300

    core: allow override cmake options

--- lib/env/cmake.sh
+++ lib/env/cmake.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export cmake_generator="Ninja"
-export cmake_build_options="-v"
-export cmake_build_type="$pkg_build_type"
+export cmake_generator="${cmake_generator:-Ninja}"
+export cmake_build_options="${cmake_build_options}"
+export cmake_build_type="${cmake_build_type:-$pkg_build_type}"
