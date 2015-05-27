8dd4f3bdf4f9b09e883766b98d43854ea5d1b856
Oleg Kolosov <bazurbat@gmail.com>
2015-05-27 17:19:23 +0300

    Adjust hisilicon toolchain env

--- lib/env/sdk.hisilicon.sh
+++ lib/env/sdk.hisilicon.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 export target_dir="$pkg_build_dir/target"
+export target_prefix="/system"
 
 export target_arch="arm"
-export target_system="arm-hisiv200-linux"
+# export target_system="arm-hisiv200-linux"
+export target_system="arm-linux-androideabi"
 
 export target_bin_dir="$target_dir/bin"
 
--- lib/toolchain/target.hisilicon.sh
+++ lib/toolchain/target.hisilicon.sh
@@ -2,14 +2,19 @@
 
 use_env target
 
-export CC="${target_system}-gcc"
-export CXX="${target_system}-g++"
-export STRIP="${target_system}-strip"
+[ "$android_toolchain_dir" ] ||
+    die "android_toolchain_dir is not set"
 
-export CFLAGS="-O2 -fomit-frame-pointer -fno-strict-aliasing -pipe"
+export AR="${android_toolchain_dir}/bin/${target_system}-ar"
+export CC="${android_toolchain_dir}/bin/${target_system}-gcc"
+export CXX="${android_toolchain_dir}/bin/${target_system}-g++"
+export STRIP="${android_toolchain_dir}/bin/${target_system}-strip"
+
+# export CFLAGS="-O2 -fomit-frame-pointer -fno-strict-aliasing -pipe"
+export CFLAGS=""
 export CXXFLAGS="$CFLAGS"
-export LDFLAGS=""
 export ASMFLAGS="$CFLAGS"
+export LDFLAGS=""
 
 export PKG_CONFIG_SYSROOT_DIR="$target_dir"
 export PKG_CONFIG_LIBDIR="$target_dir$target_prefix/lib/pkgconfig"
