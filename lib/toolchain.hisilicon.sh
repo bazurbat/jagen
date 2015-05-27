db2b2d95e084067fa62af41e31e0eaa00250fbea
Oleg Kolosov <bazurbat@gmail.com>
2015-05-27 17:18:25 +0300

    Improve hisilicon toolchain wrapper generator

--- lib/toolchain.hisilicon.sh
+++ lib/toolchain.hisilicon.sh
@@ -2,19 +2,24 @@
 
 generate_toolchain_wrappers() {
     local bin name
-    local gcc_file gcc_dir
-    local gcc_name=arm-hisiv200-linux-gnueabi-gcc
-
-    gcc_file="$(command -v "$gcc_name" 2>/dev/null)"
-    [ "$gcc_file" ] ||
-        die "Toolchain executable '$gcc_name' was not found in path"
-    gcc_dir="$(dirname "$gcc_file")"
 
     [ -d "$target_bin_dir" ] && rm -r "$target_bin_dir"
     mkdir -p "$target_bin_dir"
 
-    for bin in "$gcc_dir"/*; do
-        name="$(basename "$bin" | cut -d- -f5-)"
-        ln -s "$bin" "$target_bin_dir/${target_system}-${name}"
-    done
+    if [ -d "$sdk_toolchain_dir" ]; then
+        message "Generating HiSilicon SDK toolchain wrappers"
+
+        for bin in "$sdk_toolchain_dir"/bin/*; do
+            name="$(basename "$bin" | cut -d- -f5-)"
+            ln -s "$bin" "${target_bin_dir}/${target_system}-${name}"
+        done
+    else
+        if [ -z "$sdk_toolchain_dir" ]; then
+            warning "sdk_toolchain_dir is not set"
+        else
+            warning "sdk_toolchain_dir '$sdk_toolchain_dir' is not exists"
+        fi
+
+        warning "Skip generating HiSilicon SDK toolchain wrappers"
+    fi
 }
