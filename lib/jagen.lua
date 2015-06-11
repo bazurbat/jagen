03508dde3a539fec4e65b4010e30b563229fd0d3
Oleg Kolosov <bazurbat@gmail.com>
2015-06-11 23:41:18 +0300

    Condensed lua modules back into single file

--- lib/jagen.lua
+++ lib/jagen.lua
@@ -1,7 +1,227 @@
-require 'common'
-require 'ninja'
-local system = require 'system'
-require 'target'
+--{{{ common
+
+function copy(t)
+    local c = {}
+    for k, v in pairs(t) do
+        if type(v) == 'table' then
+            v = copy(v)
+        end
+        c[k] = v
+    end
+    return c
+end
+
+function list(t)
+    local r = {}
+    for _, v in ipairs(t or {}) do
+        table.insert(r, v)
+    end
+    return r
+end
+
+function append(...)
+    local r = {}
+    for _, arg in ipairs({...}) do
+        for _, i in ipairs(arg) do
+            table.insert(r, i)
+        end
+    end
+    return r
+end
+
+function for_each(t, f)
+    for _, v in ipairs(t or {}) do
+        f(v)
+    end
+end
+
+function map(f, t)
+    local r = {}
+    for i, v in ipairs(t or {}) do
+        table.insert(r, f(v))
+    end
+    return r
+end
+
+function find(f, t)
+    for _, v in ipairs(t or {}) do
+        if f(v) then
+            return v
+        end
+    end
+    return nil
+end
+
+function filter(pred, list)
+    local o = {}
+    for _, v in ipairs(list or {}) do
+        if pred(v) then
+            table.insert(o, v)
+        end
+    end
+    return o
+end
+
+function compose(f, g)
+    return function (...)
+        f(unpack(g(...)))
+    end
+end
+
+local function find_by_name(name, list)
+    local function by_name(x)
+        return x.name == name
+    end
+    return find(by_name, list)
+end
+
+function string.split(s, sep)
+    local o, b, e = {}
+    local init = 1
+
+    repeat
+        b, e = string.find(s, sep, init, true)
+        if not b then b = 0 end
+        table.insert(o, string.sub(s, init, b-1))
+        if e then init = e + 1 end
+    until b == 0
+
+    return o
+end
+
+function table.rest(t, start)
+    local o = {}
+    for i = start, #t do
+        table.insert(o, t[i])
+    end
+    return o
+end
+
+--}}}
+--{{{ ninja
+
+ninja = {}
+
+function ninja:format_inputs(inputs)
+    local sep = string.format(' $\n%s', jagen.format_indent(16))
+    local t = {}
+    for _, d in ipairs(inputs) do
+        table.insert(t, tostring(d))
+    end
+    return table.concat(t, sep)
+end
+
+function ninja:generate(packages, out_file, in_file)
+    local out = io.open(out_file, 'w')
+
+    out:write(string.format('builddir = %s\n\n', os.getenv('pkg_build_dir')))
+    out:write(string.format('rule command\n'))
+    out:write(string.format('    command = $command\n\n'))
+    out:write(string.format('rule script\n'))
+    out:write(string.format('    command = ' .. os.getenv('pkg_bin_dir') .. '/$script && touch $out\n\n'))
+
+    local sep = string.format(' $\n%s', jagen.format_indent(16))
+
+    for i, pkg in ipairs(packages) do
+        local pn = pkg.name
+        for j, stage in ipairs(pkg.stages or {}) do
+            local sn = stage.stage
+            local sc = stage.config
+            out:write(string.format('build %s: script', tostring(stage)))
+            if #stage.inputs > 0 then
+                out:write(' $\n' .. jagen.format_indent(16))
+                out:write(ninja:format_inputs(stage.inputs))
+            end
+            out:write('\n')
+            out:write(string.format('    script = jagen-pkg %s %s', pn, sn))
+            if sc then
+                out:write(' ', sc)
+            end
+            out:write('\n')
+        end
+        out:write("\n")
+    end
+
+    out:close()
+end
+
+--}}}
+--{{{ system
+
+local system = {}
+
+function system.mkpath(...)
+    local sep = '/'
+    local path = {}
+    for _, c in ipairs({...}) do
+        table.insert(path, c)
+    end
+    return table.concat(path, sep)
+end
+
+function system.mkdir(pathname)
+    system.exec('mkdir -p "' .. pathname .. '"')
+end
+
+function system.file_newer(file1, file2)
+    local cmd = string.format('[ "%s" -nt "%s" ]', file1, file2)
+    return os.execute(cmd) == 0
+end
+
+function system.file_older(file1, file2)
+    local cmd = string.format('[ "%s" -ot "%s" ]', file1, file2)
+    return os.execute(cmd) == 0
+end
+
+function system.exec(command, ...)
+    local cmd = { command }
+    for _, arg in ipairs({...}) do
+        table.insert(cmd, string.format('%q', arg))
+    end
+    local status = os.execute(table.concat(cmd, ' '))
+    return status
+end
+
+--}}}
+--{{{ target
+
+target = { meta = {} }
+
+function target.new(n, s, c)
+    local t = { name = n, stage = s, config = c }
+    setmetatable(t, target.meta)
+    return t
+end
+
+function target.new_from_arg(arg)
+    local name, stage, config
+    local c = string.split(arg, ':')
+
+    if c[1] and #c[1] > 0 then
+        name = c[1]
+    end
+    if c[2] and #c[2] > 0 then
+        stage = c[2]
+    end
+    if c[3] and #c[3] > 0 then
+        config = c[3]
+    end
+
+    return target.new(name, stage, config)
+end
+
+target.meta.__eq = function(a, b)
+    return a.name == b.name and
+    a.stage == b.stage and
+    a.config == b.config
+end
+
+target.meta.__tostring = function(t)
+    return table.concat({ t.name, t.stage, t.config }, '-')
+end
+
+--}}}
+--{{{ jagen
 
 jagen = {}
 
@@ -57,7 +277,7 @@ function read_package(rule)
     return rule
 end
 
-function load_rules()
+function jagen.load_rules()
     local rules = {}
     local sdk = os.getenv('pkg_sdk')
 
@@ -203,6 +423,8 @@ function jagen.generate_include_script(pkg)
     f:close()
 end
 
+--}}}
+
 command = arg[1]
 
 function jagen.build(build_file, args)
@@ -238,7 +460,7 @@ if command == 'generate' then
 
     if system.file_older(build_file, rules_file) or jagen.is_debug() then
         jagen.message("Generating build rules")
-        local packages = load_rules()
+        local packages = jagen.load_rules()
         ninja:generate(packages, arg[2], arg[3])
         for_each(packages, jagen.generate_include_script)
     end
