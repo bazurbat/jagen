b0421194912949470ff02526d26fc0be7e8b1b27
Oleg Kolosov <bazurbat@gmail.com>
2015-06-19 00:52:55 +0300

    Remove jagen env

--- env.sh
+++ env.sh
@@ -5,8 +5,6 @@ export jagen_root="$PWD"
 . "$jagen_root/lib/env.sh" ||
     { echo "Failed to load environment"; exit 1; }
 
-use_env jagen
-
 p_path_prepend "$target_bin_dir"
 p_path_prepend "$pkg_private_dir/bin"
 p_path_prepend "$jagen_root/bin"
