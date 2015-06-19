a6a4fb81b4f94460ac1d21f5078361e24c2514fd
Oleg Kolosov <bazurbat@gmail.com>
2015-06-19 12:09:16 +0300

    Make cmake_build_type synonymous to pkg_build_type

--- lib/env/cmake.sh
+++ lib/env/cmake.sh
@@ -2,4 +2,4 @@
 
 export cmake_generator="${cmake_generator:-Ninja}"
 export cmake_build_options="${cmake_build_options}"
-export cmake_build_type="${cmake_build_type:-$pkg_build_type}"
+export cmake_build_type="$pkg_build_type"
