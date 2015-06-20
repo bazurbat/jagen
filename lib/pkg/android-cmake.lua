acfa5dadb43aa94c480a5518a957cffe7da746bc
Oleg Kolosov <bazurbat@gmail.com>
2015-06-20 20:01:29 +0300

    Add hisilicon lua rules

---
+++ lib/pkg/android-cmake.lua
@@ -0,0 +1,7 @@
+package {
+    name = 'android-cmake',
+    source = {
+        type     = 'git',
+        location = 'https://github.com/taka-no-me/android-cmake.git'
+    }
+}
---
+++ lib/rules.hisilicon.lua
@@ -0,0 +1,72 @@
+package {
+    name = 'ast-files'
+}
+
+package {
+    name = 'make',
+    { 'build',   'host' },
+    { 'install', 'host' }
+}
+
+package {
+    name = 'android-cmake'
+}
+
+package {
+    name = 'libuv',
+    { 'build',   'target' },
+    { 'install', 'target',
+        { 'firmware', 'unpack' }
+    }
+}
+
+package {
+    name = 'ffmpeg',
+    { 'build',   'target',
+        { 'ast-files', 'unpack' }
+    },
+    { 'install', 'target',
+        { 'firmware',  'unpack' }
+    }
+}
+
+package {
+    name = 'chicken',
+    source = {
+        branch = 'cmake'
+    },
+    { 'build',   'host' },
+    { 'install', 'host' },
+    { 'build',   'target',
+        { 'chicken',  'install', 'host' }
+    },
+    { 'install', 'target',
+        { 'firmware', 'unpack'          }
+    }
+}
+
+package {
+    name = 'astindex',
+    { 'unpack', { 'karaoke-player', 'unpack' } }
+}
+
+package {
+    name = 'karaoke-player',
+    { 'build', 'target',
+        { 'android-cmake', 'install', 'unpack' },
+        { 'astindex',      'unpack'            },
+        { 'chicken',       'install', 'target' },
+        { 'ffmpeg',        'install', 'target' },
+        { 'libuv',         'install', 'target' },
+    },
+    { 'install', 'target' }
+}
+
+package {
+    name = 'firmware',
+    { 'install',
+        { 'karaoke-player', 'install', 'target' }
+    },
+    { 'strip'  },
+    { 'deploy' }
+}
