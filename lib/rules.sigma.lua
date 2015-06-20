042115e7965f9e98c4e83703261129e31e35779d
Oleg Kolosov <bazurbat@gmail.com>
2015-06-20 17:45:49 +0300

    Remove duplicated source definitions from rules

--- lib/rules.sigma.lua
+++ lib/rules.sigma.lua
@@ -31,18 +31,12 @@ end
 -- base
 
 package {
-    name = 'ast-files',
-    source = {
-        type = 'git',
-        location = 'git@bitbucket.org:art-system/files.git'
-    }
+    name = 'ast-files'
 }
 
 package {
     name = 'linux',
     source = {
-        type = 'git',
-        location = 'git@bitbucket.org:art-system/linux.git',
         branch = 'ast50'
     }
 }
@@ -54,7 +48,6 @@ package {
 
 package {
     name = 'ucode',
-    source = 'mruafw_SMP8654F_3_11_3_prod.tgz',
     { 'unpack',  { 'mrua',     'build'  } },
     { 'install', { 'firmware', 'unpack' } }
 }
@@ -63,7 +56,6 @@ package {
 
 package {
     name = 'make',
-    source = 'make-3.80.tar.bz2',
     { 'build',   'host' },
     { 'install', 'host' }
 }
@@ -72,11 +64,6 @@ package {
 
 package {
     name = 'libtool',
-    source = 'libtool-2.4.3.tar.xz',
-    patches = {
-        { 'libtool-2.4.3-no-clean-gnulib',   1 },
-        { 'libtool-2.4.3-test-cmdline_wrap', 1 }
-    },
     { 'build'   },
     { 'install' }
 }
@@ -85,10 +72,6 @@ package {
 
 package {
     name = 'utils',
-    source = {
-        type = 'git',
-        location = 'git@bitbucket.org:art-system/sigma-utils.git'
-    },
     { 'build',   'host' },
     { 'install', 'host' },
     { 'build',   'target',
@@ -103,8 +86,6 @@ package {
 rootfs_package {
     name = 'ezboot',
     source = {
-        type = 'git',
-        location = 'git@bitbucket.org:art-system/sigma-ezboot.git',
         branch = "sdk4"
     }
 }
@@ -114,19 +95,16 @@ rootfs_package {
 if jagen.flag('debug') then
     package {
         name = 'gdb',
-        source = 'gdb-7.9.tar.xz',
         { 'build',   'host' },
         { 'install', 'host' }
     }
 
     rootfs_package {
-        name = 'gdbserver',
-        source = 'gdb-7.9.tar.xz'
+        name = 'gdbserver'
     }
 
     rootfs_package {
-        name = 'strace',
-        source = 'strace-4.8.tar.xz'
+        name = 'strace'
     }
 end
 
@@ -134,10 +112,6 @@ end
 
 package {
     name = 'rootfs',
-    source = {
-        type = 'git',
-        location = 'git@bitbucket.org:art-system/sigma-rootfs.git'
-    },
     { 'build',
         { 'ast-files',  'unpack'            },
         { 'xsdk',       'unpack'            },
@@ -158,46 +132,36 @@ package {
 
 rootfs_package {
     name = 'busybox',
-    source = 'busybox-1.22.1.tar.bz2',
     { 'patch', { 'ast-files', 'unpack' } }
 }
 
 rootfs_package {
-    name = 'ntpclient',
-    source = 'ntpclient-2010.tar.gz'
+    name = 'ntpclient'
 }
 
 rootfs_package {
     name = 'util-linux',
-    source = 'util-linux-2.23.2.tar.xz',
-    patches = {
-        { 'util-linux-2.23.2', 1 }
-    },
     { 'patch', { 'libtool', 'install' } }
 }
 
 -- gpgme
 
 rootfs_package {
-    name = 'libgpg-error',
-    source = 'libgpg-error-1.17.tar.bz2'
+    name = 'libgpg-error'
 }
 
 rootfs_package {
     name = 'libassuan',
-    source = 'libassuan-2.1.2.tar.bz2',
     { 'build', { 'libgpg-error', 'install' } }
 }
 
 rootfs_package {
     name = 'gpgme',
-    source = 'gpgme-1.5.1.tar.bz2',
     { 'build', { 'libassuan', 'install' } }
 }
 
 rootfs_package {
-    name = 'gnupg',
-    source = 'gnupg-1.4.18.tar.bz2'
+    name = 'gnupg'
 }
 
 -- kernel
@@ -205,8 +169,6 @@ rootfs_package {
 package {
     name = 'kernel',
     source = {
-        type = 'git',
-        location = 'git@bitbucket.org:art-system/sigma-kernel.git',
         branch = 'sigma-2.6'
     },
     { 'build',
@@ -219,24 +181,16 @@ package {
 }
 
 kernel_package {
-    name = 'ralink',
-    source = 'DPO_RT5572_LinuxSTA_2.6.1.3_20121022.tar.bz2',
-    patches = {
-        { 'DPO_RT5572_LinuxSTA_2.6.1.3_20121022-no-tftpboot', 1 },
-        { 'DPO_RT5572_LinuxSTA_2.6.1.3_20121022-encrypt',     1 }
-    }
+    name = 'ralink'
 }
 
 kernel_package {
-    name = 'loop-aes',
-    source = 'loop-AES-v3.7b.tar.bz2'
+    name = 'loop-aes'
 }
 
 package {
     name = 'mrua',
     source = {
-        type = 'git',
-        location = 'git@bitbucket.org:art-system/sigma-mrua.git',
         branch = 'sigma-2.6'
     },
     { 'build',   { 'kernel',   'build'  } },
@@ -247,8 +201,6 @@ package {
 package {
     name = 'chicken',
     source = {
-        type = 'git',
-        location = 'https://github.com/bazurbat/chicken-scheme.git',
         branch = 'cmake'
     },
     { 'build',   'host' },
@@ -264,8 +216,6 @@ package {
 package {
     name = 'chicken-eggs',
     source = {
-        type = 'git',
-        location = 'https://github.com/bazurbat/chicken-eggs.git',
         branch = 'master'
     },
     { 'install', 'host',
@@ -281,7 +231,6 @@ package {
 
 package {
     name = 'libuv',
-    source = 'libuv-1.4.2.tar.gz',
     { 'build',   'host' },
     { 'install', 'host' },
     { 'build',   'target' },
@@ -292,56 +241,34 @@ package {
 
 firmware_package {
     name = 'dbus',
-    source = 'dbus-1.6.18.tar.gz',
     { 'build', { 'expat', 'install' } }
 }
 
 firmware_package {
-    name = 'expat',
-    source = 'expat-2.1.0.tar.gz'
+    name = 'expat'
 }
 
 firmware_package {
-    name = 'freetype',
-    source = 'freetype-2.5.0.1.tar.bz2',
-    patches = {
-        { 'freetype-2.3.2-enable-valid',   1 },
-        { 'freetype-2.4.11-sizeof-types',  1 },
-        { 'freetype-2.4.12-clean-include', 1 }
-    }
+    name = 'freetype'
 }
 
 firmware_package {
-    name = 'rsync',
-    source = 'rsync-3.1.1.tar.gz'
+    name = 'rsync'
 }
 
 firmware_package {
     name = 'sqlite',
-    source = 'sqlite-autoconf-3080403.tar.gz',
-    patches = {
-        { 'sqlite-3.8.1-autoconf-dlopen_check', 0 }
-    },
     { 'patch', { 'libtool', 'install' } }
 }
 
 firmware_package {
     name = 'libnl',
-    source = 'libnl-3.2.25.tar.gz',
-    patches = {
-        { 'libnl-3.2.20-rtnl_tc_get_ops', 1 },
-        { 'libnl-3.2.20-cache-api',       1 },
-    },
     { 'build'   },
     { 'install' }
 }
 
 firmware_package {
     name = 'wpa_supplicant',
-    source = 'wpa_supplicant-2.2.tar.gz',
-    patches = {
-        { 'wpa_supplicant-2.2-do-not-call-dbus-functions-with-NULL-path', 1 }
-    },
     { 'build',
         { 'dbus',  'install' },
         { 'libnl', 'install' }
@@ -349,32 +276,27 @@ firmware_package {
 }
 
 firmware_package {
-    name = 'zlib',
-    source = 'zlib-1.2.8.tar.gz'
+    name = 'zlib'
 }
 
 firmware_package {
     name = 'libpng',
-    source = 'libpng-1.6.17.tar.xz',
     { 'build',
         { 'zlib', 'install' }
     }
 }
 
 firmware_package {
-    name = 'xtables',
-    source = 'iptables-1.4.21.tar.bz2'
+    name = 'xtables'
 }
 
 firmware_package {
     name = 'xtables-addons',
-    source = 'xtables-addons-1.47.1.tar.xz',
     { 'build', { 'xtables', 'install' } }
 }
 
 package {
     name = 'ffmpeg',
-    source = 'ffmpeg-2.2.1.tar.bz2',
     { 'build',   'host',
         { 'ast-files', 'unpack' }
     },
@@ -389,27 +311,17 @@ package {
 
 package {
     name = 'soundtouch',
-    source = 'soundtouch-1.8.0.tar.gz',
     { 'build' },
     { 'install', { 'firmware', 'unpack' } }
 }
 
 package {
     name = 'astindex',
-    source = {
-        type = 'hg',
-        location = 'ssh://hg@bitbucket.org/art-system/astindex',
-        directory = 'karaoke-player/source/astindex'
-    },
     { 'unpack', { 'karaoke-player', 'unpack' } }
 }
 
 package {
     name = 'karaoke-player',
-    source = {
-        type = 'hg',
-        location = 'ssh://hg@bitbucket.org/art-system/karaoke-player'
-    },
     { 'build', 'host',
         { 'astindex',     'unpack'            },
         { 'ffmpeg',       'install', 'host'   },
@@ -463,21 +375,12 @@ package {
 
 package {
     name = 'libffi',
-    source = 'libffi-3.1.tar.gz',
-    patches = {
-        { 'libffi-3.1-execstack', 0 },
-        { 'libffi-3.1-typing_error', 0 },
-    },
     { 'build',   { 'libtool',  'install' } },
     { 'install', { 'firmware', 'unpack'  } }
 }
 
 package {
     name = 'glib',
-    source = 'glib-2.40.2.tar.xz',
-    patches = {
-        { 'glib-2.40.0-external-gdbus-codegen', 1 }
-    },
     { 'patch',
         { 'libtool',  'install' }
     },
@@ -492,7 +395,6 @@ package {
 
 package {
     name = 'connman',
-    source = 'connman-1.28.tar.xz',
     { 'build',
         { 'libtool',        'install' },
         { 'dbus',           'install' },
@@ -506,7 +408,6 @@ package {
 
 package {
     name = 'fribidi',
-    source = 'fribidi-0.19.6.tar.bz2',
     { 'build' },
     { 'install',
         { 'firmware', 'unpack' }
@@ -515,7 +416,6 @@ package {
 
 package {
     name = 'fontconfig',
-    source = 'fontconfig-2.11.1.tar.bz2',
     { 'build' },
     { 'install',
         { 'firmware', 'unpack' }
@@ -524,7 +424,6 @@ package {
 
 package {
     name = 'libass',
-    source = 'libass-0.10.2.tar.xz',
     { 'build',
         { 'fribidi', 'install' },
         { 'fontconfig', 'install' }
