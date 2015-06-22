e735597f241fdb5ac96f56354737d15de79a36ca
Oleg Kolosov <bazurbat@gmail.com>
2015-06-22 23:22:49 +0300

    Remove ast-files pkg script

--- lib/pkg/ast-files.lua
+++ lib/pkg/ast-files.lua
@@ -1,5 +1,8 @@
 package {
     name   = 'ast-files',
+    build = {
+        dir = '$p_work_dir/build${p_config:+-$p_config}'
+    },
     source = {
         type     = 'git',
         location = 'git@bitbucket.org:art-system/files.git'
--- lib/pkg/ast-files.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-p_source_dir="$pkg_src_dir/files"
-p_build_dir="$p_work_dir/build${p_config:+-$p_config}"
