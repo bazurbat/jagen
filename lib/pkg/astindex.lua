a53fe854b4930aa7c7349fcf2e7b83fe58c54087
Oleg Kolosov <bazurbat@gmail.com>
2015-06-22 23:22:56 +0300

    Remove astindex pkg script

--- lib/pkg/astindex.lua
+++ lib/pkg/astindex.lua
@@ -1,5 +1,8 @@
 package {
     name   = 'astindex',
+    build = {
+        dir = '$p_work_dir/build${p_config:+-$p_config}'
+    },
     source = {
         type      = 'hg',
         location  = 'ssh://hg@bitbucket.org/art-system/astindex',
--- lib/pkg/astindex.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-p_source_dir="$pkg_src_dir/karaoke-player/source/astindex"
-p_build_dir="$p_work_dir/build${p_config:+-$p_config}"
