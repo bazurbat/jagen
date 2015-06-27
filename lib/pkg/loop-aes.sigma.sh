6212d1c9549c1877560330d5889fbcf4f00c9e0e
Oleg Kolosov <bazurbat@gmail.com>
2015-06-27 23:28:13 +0300

    Add compiler to path for loop-aes

--- lib/pkg/loop-aes.sigma.sh
+++ lib/pkg/loop-aes.sigma.sh
@@ -5,6 +5,8 @@ use_toolchain target
 export ARCH=mips
 
 pkg_build() {
+    PATH="$jagen_toolchain_dir/bin:$PATH"
+
     p_run make \
         LINUX_SOURCE="$LINUX_KERNEL" \
         KBUILD_OUTPUT="$LINUX_KERNEL" \
