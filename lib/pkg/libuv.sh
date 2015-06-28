0bf5db687a96711d6e83f81485084ffc58bcecbe
Oleg Kolosov <bazurbat@gmail.com>
2015-06-28 18:33:12 +0300

    Add back overriding target scripts for libuv

--- lib/pkg/libuv.sh
+++ lib/pkg/libuv.sh
@@ -16,3 +16,16 @@ pkg_install_host() {
     p_run make DESTDIR="$host_dir" install
     p_fix_la "${host_dir}${host_prefix}/lib/libuv.la" "$host_dir"
 }
+
+pkg_build_target() {
+    p_run "$p_source_dir/configure" \
+        --host="$target_system" \
+        --prefix="$target_prefix"
+
+    p_run make
+}
+
+pkg_install_target() {
+    p_run make DESTDIR="$target_dir" install
+    p_fix_la "${target_dir}${target_prefix}/lib/libuv.la" "$target_dir"
+}
