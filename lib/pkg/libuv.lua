4cd671e24a042cd1b4995ed82ead6377aec836c1
Oleg Kolosov <bazurbat@gmail.com>
2015-07-02 01:51:18 +0300

    Add libs list to libuv

--- lib/pkg/libuv.lua
+++ lib/pkg/libuv.lua
@@ -2,7 +2,8 @@ package {
     name   = 'libuv',
     build  = {
         type = 'GNU',
-        dir  = '$p_work_dir/build${p_config:+-$p_config}'
+        dir  = '$p_work_dir/build${p_config:+-$p_config}',
+        libs = { 'uv' }
     },
     source = 'libuv-1.4.2.tar.gz'
 }
