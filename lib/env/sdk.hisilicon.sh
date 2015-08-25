0717154e1be37bfb417e8207757757a72b80bd66
Oleg Kolosov <bazurbat@gmail.com>
2015-08-25 14:12:59 +0300

    Change hisilicon SDK to inherit android

--- lib/env/sdk.hisilicon.sh
+++ lib/env/sdk.hisilicon.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 
-export JAVA_HOME="/usr/lib/jvm/sun-jdk-1.6"
-export JAVAC="$JAVA_HOME/bin/javac"
-# export _JAVA_OPTIONS="-Xms1024m -Xmx2048m -XX:MaxPermSize=256m"
-
-export TOP="$sdk_top_dir"
-
-export USE_CCACHE=1
-
-add_PATH "$JAVA_HOME/bin"
+use_env sdk android
