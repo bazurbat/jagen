bf1ca27ae1c1343cbfc1de356e72574b42946656
Oleg Kolosov <bazurbat@gmail.com>
2015-08-26 15:55:06 +0300

    Remove libnl dependency from wpa_supplicant

--- lib/pkg/wpa_supplicant.lua
+++ lib/pkg/wpa_supplicant.lua
@@ -5,6 +5,6 @@ package {
         { 'wpa_supplicant-2.2-do-not-call-dbus-functions-with-NULL-path', 1 }
     },
     { 'build',
-        needs = { 'dbus', 'libnl' }
+        needs = { 'dbus' }
     }
 }
