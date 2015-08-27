b7e53265b5e83e13ed0260f117d36e63ea21b8aa
Oleg Kolosov <bazurbat@gmail.com>
2015-08-27 12:30:22 +0300

    Add error about bash required in hisi build

--- lib/env/sdk.hisilicon.sh
+++ lib/env/sdk.hisilicon.sh
@@ -5,6 +5,12 @@ use_env sdk android
 target_system="arm-hisiv200-linux"
 toolchain_bin_dir="${target_dir}/bin"
 
+if [ ! "$BASH" ]; then
+    error "Android build requires Bash shell, please run it and source the \
+environment again."
+    return 22
+fi
+
 make_toolchain() {
     : ${jagen_toolchain_dir:?}
 
