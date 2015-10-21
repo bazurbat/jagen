75fb630e2234c1c6b4ab8ac7f4a6eba957d271af
Oleg Kolosov <bazurbat@gmail.com>
2015-10-21 19:38:58 +0300

    android: call make-standalone-toolchain with bash explicitly

--- lib/env/sdk.android.sh
+++ lib/env/sdk.android.sh
@@ -28,7 +28,7 @@ make_toolchain() {
     rm -fr "$toolchain_bin_dir"
     mkdir -p "$toolchain_bin_dir"
 
-    "$jagen_toolchain_dir/build/tools/make-standalone-toolchain.sh" \
+    bash "$jagen_toolchain_dir/build/tools/make-standalone-toolchain.sh" \
         --system="linux-x86_64" \
         --platform="$target_platform" \
         --toolchain="$target_toolchain" \
