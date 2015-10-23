2fcb907d98545e9f4c7b83eb4793383295aa39f6
rs <rs@dev.artsystem>
2015-10-23 14:16:06 +0300

    soundtouch updated to 1.9.2

--- lib/pkg/soundtouch.lua
+++ lib/pkg/soundtouch.lua
@@ -1,6 +1,6 @@
 package {
     name   = 'soundtouch',
-    source = 'soundtouch-1.8.0.tar.gz',
+    source = 'soundtouch-1.9.2.tar.gz',
     build  = {
         type = 'GNU',
         options = '--enable-integer-samples=yes --enable-x86-optimizations=no',
