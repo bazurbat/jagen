ded8d228d69a3aad3fc535ea0b1786015cd6761d
Oleg Kolosov <bazurbat@gmail.com>
2015-11-06 16:10:09 +0300

    Clean up hisilicon rules

--- lib/rules.hisilicon.lua
+++ lib/rules.hisilicon.lua
@@ -1,9 +1,11 @@
+-- HiSilicon SDK
+
 package { 'make', 'host',
     source = 'make-3.81.tar.bz2'
 }
 
-package { 'hisilicon', 'host',
+package { 'hisilicon',
     { 'build',
-        needs = { 'make' }
+        { 'make', 'build', 'host' }
     }
 }
