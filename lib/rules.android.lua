99be502f019dc4bab93e7e918086c445d1eeb4e4
Oleg Kolosov <bazurbat@gmail.com>
2015-11-09 19:25:59 +0300

    Enable karaoke-player and depdendencies for android build

--- lib/rules.android.lua
+++ lib/rules.android.lua
@@ -2,18 +2,15 @@
 
 package { 'android-cmake' }
 
--- package { 'libtool', 'host' }
+package { 'libtool', 'host' }
 
--- package { 'chicken', 'host',
---     source = { branch = 'stable-cmake' }
--- }
+package { 'chicken', 'host' }
 
--- package { 'chicken-eggs', 'host',
---     source = { branch = 'newchicken' },
---     { 'install',
---         { 'chicken', 'install', 'host' }
---     }
--- }
+package { 'chicken-eggs', 'host',
+    { 'install',
+        { 'chicken', 'install', 'host' }
+    }
+}
 
 local function firmware_package(rule)
     rule.config = 'target'
@@ -24,11 +21,11 @@ end
 package { 'firmware',
     { 'install',
         { 'hi-utils', 'install', 'target' },
-        -- { 'chicken',        'install', 'target' },
-        -- { 'chicken-eggs',   'install', 'target' },
-        -- { 'ffmpeg',         'install', 'target' },
-        -- { 'karaoke-player', 'install', 'target' },
-        -- { 'libuv',          'install', 'target' },
+        { 'chicken',        'install', 'target' },
+        { 'chicken-eggs',   'install', 'target' },
+        { 'ffmpeg',         'install', 'target' },
+        { 'karaoke-player', 'install', 'target' },
+        { 'libuv',          'install', 'target' },
     },
     { 'strip'  },
     { 'deploy' }
@@ -36,41 +33,41 @@ package { 'firmware',
 
 firmware_package { 'hi-utils' }
 
--- firmware_package { 'chicken',
---     source = { branch = 'stable-cmake' },
---     { 'build', { 'chicken', 'install', 'host' } }
--- }
+firmware_package { 'chicken',
+    { 'build',
+        { 'chicken', 'install', 'host' }
+    }
+}
 
--- firmware_package { 'chicken-eggs',
---     source = { branch = 'newchicken' },
---     { 'install',
---         { 'chicken', 'install', 'target' },
---         { 'sqlite',  'install', 'target' },
---     }
--- }
+firmware_package { 'chicken-eggs',
+    { 'install',
+        { 'chicken-eggs', 'install', 'host'   },
+        { 'chicken',      'install', 'target' },
+        { 'sqlite',       'install', 'target' },
+    }
+}
 
--- firmware_package { 'sqlite' }
+firmware_package { 'sqlite' }
 
--- firmware_package { 'ffmpeg' }
+firmware_package { 'ffmpeg' }
 
--- firmware_package { 'karaoke-player',
---     source = { branch = 'newchicken' },
---     { 'build',
---         { 'astindex',     'unpack'            },
---         { 'chicken-eggs', 'install', 'host'   },
---         { 'chicken-eggs', 'install', 'target' },
---         { 'ffmpeg',       'install', 'target' },
---         { 'libuv',        'install', 'target' },
---     },
---     { 'install' }
--- }
+firmware_package { 'karaoke-player',
+    { 'build',
+        { 'astindex',     'unpack'            },
+        { 'chicken-eggs', 'install', 'host'   },
+        { 'chicken-eggs', 'install', 'target' },
+        { 'ffmpeg',       'install', 'target' },
+        { 'libuv',        'install', 'target' },
+    },
+    { 'install' }
+}
 
--- package { 'astindex',
---     { 'unpack', { 'karaoke-player', 'unpack' } }
--- }
+package { 'astindex',
+    { 'unpack', { 'karaoke-player', 'unpack' } }
+}
 
--- firmware_package { 'libuv',
---     build  = {
---         options = '--disable-static'
---     }
--- }
+firmware_package { 'libuv',
+    build  = {
+        options = '--disable-static'
+    }
+}
