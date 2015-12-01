c8108abd169b03d41c3270ed46d3f2a167180522
Oleg Kolosov <bazurbat@gmail.com>
2015-12-01 17:49:50 +0300

    Clean up karaoke-player target build step

--- lib/pkg/karaoke-player.sh
+++ lib/pkg/karaoke-player.sh
@@ -12,32 +12,31 @@ jagen_pkg_build_host() {
 }
 
 jagen_pkg_build_target() {
+    local IFS="$(printf '\n\t')"
+    local S="$(printf '\t')"
+    local A="-G$jagen_cmake_generator"
+    A="$A$S-DCMAKE_SYSTEM_NAME=Linux"
+    A="$A$S-DCMAKE_BUILD_TYPE=$jagen_cmake_build_type"
+    A="$A$S-DCMAKE_INSTALL_PREFIX=${jagen_target_dir}${jagen_target_prefix}"
+    A="$A$S-DCHICKEN_COMPILER=$jagen_host_dir/bin/chicken"
+    A="$A$S-DCHICKEN_INTERPRETER=$jagen_host_dir/bin/csi"
+    A="$A$S-DTARGET_BOARD=$target_board"
+
     case $jagen_sdk in
         sigma)
-            pkg_run cmake -G"$jagen_cmake_generator" \
-                -DCMAKE_BUILD_TYPE="$jagen_cmake_build_type" \
-                -DCMAKE_SYSTEM_NAME="Linux" \
-                -DCMAKE_INSTALL_PREFIX="${jagen_target_dir}${jagen_target_prefix}" \
+            pkg_run cmake $A \
                 -DCMAKE_FIND_ROOT_PATH="${jagen_target_dir}${jagen_target_prefix}" \
                 -DSIGMA_SDK_DIR="$jagen_src_dir/sigma-mrua" \
                 -DSIGMA_ROOTFS_DIR="$jagen_src_dir/sigma-rootfs" \
-                -DCHICKEN_COMPILER="$jagen_host_dir/bin/chicken" \
-                -DCHICKEN_INTERPRETER="$jagen_host_dir/bin/csi" \
-                -DTARGET_BOARD="$target_board" \
                 "$pkg_source_dir"
             ;;
         android)
-            pkg_run cmake -G"$jagen_cmake_generator" \
+            pkg_run cmake $A \
                 -DCMAKE_TOOLCHAIN_FILE="$jagen_src_dir/android-cmake/android.toolchain.cmake" \
                 -DANDROID_STANDALONE_TOOLCHAIN="${jagen_target_dir}/${target_toolchain}" \
-                -DCMAKE_BUILD_TYPE="$jagen_cmake_build_type" \
-                -DCMAKE_INSTALL_PREFIX="${jagen_target_dir}${jagen_target_prefix}" \
                 -DPKG_CONFIG_EXECUTABLE="${jagen_host_dir}/bin/pkgconf" \
-                -DCHICKEN_COMPILER="$jagen_host_dir/bin/chicken" \
-                -DCHICKEN_INTERPRETER="$jagen_host_dir/bin/csi" \
                 -DHISILICON_ROOT_DIR="$jagen_sdk_dir" \
                 -DHISILICON_OUT_DIR="$sdk_out_dir" \
-                -DTARGET_BOARD="$target_board" \
                 "$pkg_source_dir"
             ;;
     esac
