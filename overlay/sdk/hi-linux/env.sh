47e53cafadd53036977fa8486f01903d1e9f9ca4
Oleg Kolosov <bazurbat@gmail.com>
2015-12-02 18:47:32 +0300

    Add toolchain sysroot and prefix to hi-linux sdk env

--- overlay/sdk/hi-linux/env.sh
+++ overlay/sdk/hi-linux/env.sh
@@ -5,3 +5,5 @@ jagen_sdk='hi-linux'
 target_system="arm-hisiv200-linux"
 
 toolchain_dir="${jagen_target_dir}"
+toolchain_sysroot="${jagen_toolchain_dir}/target"
+toolchain_prefix="${toolchain_dir}/bin/${target_system}-"
