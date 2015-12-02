d104bb642517e1c049e21fff1b5528e6159a9099
Oleg Kolosov <bazurbat@gmail.com>
2015-12-02 18:51:54 +0300

    Add --disable-static to default libuv build options

--- lib/pkg/libuv.lua
+++ lib/pkg/libuv.lua
@@ -1,9 +1,10 @@
 package {
     name   = 'libuv',
     build  = {
-        type = 'GNU',
-        dir  = '$pkg_work_dir/build${pkg_config:+-$pkg_config}',
-        libs = { 'uv' }
+        type    = 'GNU',
+        dir     = '$pkg_work_dir/build${pkg_config:+-$pkg_config}',
+        options = '--disable-static',
+        libs    = { 'uv' }
     },
     source = 'libuv-1.4.2.tar.gz'
 }
