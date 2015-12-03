6ea96b10d7d91e3338511481aeef6c9fb789e493
Oleg Kolosov <bazurbat@gmail.com>
2015-12-04 01:03:51 +0300

    Define jagen_toolchain_prefix in global target toolchain env

--- lib/toolchain/target.sh
+++ lib/toolchain/target.sh
@@ -17,6 +17,15 @@ export LDFLAGS=""
 export PKG_CONFIG_SYSROOT_DIR="${jagen_target_dir}"
 export PKG_CONFIG_LIBDIR="${jagen_target_dir}${jagen_target_prefix}/lib/pkgconfig"
 
-: ${jagen_target_toolchain_dir:?}
+if [ -z "$jagen_target_toolchain_dir" ]; then
+    jagen_target_toolchain_dir="$jagen_toolchain_dir/bin"
+fi
 
-add_PATH "$jagen_target_toolchain_dir"
+if [ -z "$jagen_toolchain_prefix" ]; then
+    : ${jagen_target_system:?}
+    jagen_toolchain_prefix="${jagen_target_toolchain_dir}/${jagen_target_system}-"
+fi
+
+if [ -d "$jagen_target_toolchain_dir" ]; then
+    add_PATH "$jagen_target_toolchain_dir"
+fi
--- overlay/sdk/hi-linux/env.sh
+++ overlay/sdk/hi-linux/env.sh
@@ -7,4 +7,3 @@ jagen_target_system="arm-hisiv200-linux"
 jagen_target_toolchain_dir="${jagen_target_dir}/bin"
 
 jagen_toolchain_sysroot="${jagen_toolchain_dir}/target"
-jagen_toolchain_prefix="${jagen_target_toolchain_dir}/${jagen_target_system}-"
