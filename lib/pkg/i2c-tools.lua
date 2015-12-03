5742d7f65d42e6a6bc2dbc8d73d990fc9fdf01c3
Oleg Kolosov <bazurbat@gmail.com>
2015-12-04 01:08:23 +0300

    Remove i2c-tools

--- lib/pkg/i2c-tools.lua
+++
@@ -1,7 +0,0 @@
-package {
-    name   = 'i2c-tools',
-    build  = {
-        type = 'GNU',
-    },
-    source = 'i2c-tools-3.1.2.tar.bz2'
-}
--- lib/pkg/i2c-tools.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-jagen_pkg_build() {
-    pkg_run make
-}
-
-jagen_pkg_install() {
-    pkg_run make DESTDIR="$jagen_target_dir" prefix="$jagen_target_prefix" install
-}
