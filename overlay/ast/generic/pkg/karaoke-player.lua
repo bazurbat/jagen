6674e442ce040ccda9e2062c788304fb7c072285
Oleg Kolosov <bazurbat@gmail.com>
2015-12-04 01:19:01 +0300

    Move specific packages to their respective overlays

--- overlay/ast/generic/pkg/karaoke-player.lua
+++ overlay/ast/generic/pkg/karaoke-player.lua
@@ -1,4 +1,9 @@
 package {
+    name   = 'karaoke-player',
+    build = {
+        type = 'CMake',
+        dir  = '$pkg_work_dir/build${pkg_config:+-$pkg_config}'
+    },
     source = {
         type     = 'hg',
         location = 'ssh://hg@bitbucket.org/art-system/karaoke-player',
