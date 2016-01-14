0ae6a2f24653d19b1bc91d5c1413d33e9a635df7
Oleg Kolosov <bazurbat@gmail.com>
2016-01-14 17:30:04 +0300

    Adjust target env

--- overlay/ast/ast2xx/env.sh
+++ overlay/ast/ast2xx/env.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-jagen_target_arch="arm"
-jagen_target_system="arm-linux-androideabi"
-jagen_target_platform="android-17"
-jagen_target_toolchain="${jagen_target_system}-4.6"
-jagen_target_toolchain_dir="${jagen_target_dir}/${jagen_target_toolchain}/bin"
-
 jagen_target_board='ast2xx'
+
+jagen_target_platform="android-17"
+jagen_android_product="Hi3719CV100"
---
+++ overlay/target/mipsel-linux-gnu/env.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+jagen_target_arch="mips"
