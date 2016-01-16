5cb0d8454cd4579a4d76d2f121a6e206898026f0
Oleg Kolosov <bazurbat@gmail.com>
2016-01-16 21:19:43 +0300

    Fix ast2xx config to build new android firmware

--- lib/config/ast2xx/env.sh
+++ lib/config/ast2xx/env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-jagen_overlays='ast/board/ast2xx toolchain/arm-hisiv200-linux sdk/android product/android ast/common ast/product/android'
+jagen_overlays='ast/board/ast2xx toolchain/arm-linux-androideabi sdk/android ast/common ast/product/android-firmware'
 jagen_toolchain_dir='$jagen_base_dir/toolchain/android-ndk-r10d'
 jagen_sdk_dir='$jagen_src_dir/android'
---
+++ overlay/ast/product/android-firmware/pkg/karaoke-player.lua
@@ -0,0 +1,5 @@
+package {
+    source = {
+        branch = 'master'
+    }
+}
