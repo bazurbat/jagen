d9ddfc1d24e31ce97f4b0a0d54c5f2fd304d5d83
Oleg Kolosov <bazurbat@gmail.com>
2016-01-18 01:51:22 +0300

    Set sdk before toolchain in default configs

--- lib/config/ast2xx/env.sh
+++ lib/config/ast2xx/env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-jagen_overlays='ast/board/ast2xx toolchain/arm-linux-androideabi sdk/android ast/common ast/product/android-firmware'
+jagen_overlays='ast/board/ast2xx sdk/android toolchain/arm-linux-androideabi ast/common ast/product/android-firmware'
 jagen_toolchain_dir='$jagen_base_dir/toolchain/android-ndk-r10d'
 jagen_sdk_dir='$jagen_src_dir/android'
