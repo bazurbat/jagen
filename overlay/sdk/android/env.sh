7d7d844b10dd7b05f9d38ddd034fc0e074accc09
Oleg Kolosov <bazurbat@gmail.com>
2016-01-18 01:51:09 +0300

    Override target dir and target prefix in android sdk

--- overlay/sdk/android/env.sh
+++ overlay/sdk/android/env.sh
@@ -5,12 +5,12 @@ jagen_sdk='android'
 # Many Android build and utility scripts assume Bash shell
 jagen_shell="/bin/bash"
 
+: ${jagen_sdk_dir:?}
+: ${jagen_android_product:?}
+
+jagen_target_dir="$jagen_sdk_dir/out/target/product/$jagen_android_product"
 jagen_target_prefix="/system"
 
 if in_flags ccache; then
     export USE_CCACHE=1
-fi
-
-if [ "$jagen_android_product" ]; then
-    jagen_install_dir="$jagen_sdk_dir/out/target/product/$jagen_android_product"
 fi
