c2f798a9b7826dc8eabfbf1ff2610b58c2e6c7bb
Oleg Kolosov <bazurbat@gmail.com>
2016-01-18 01:46:05 +0300

    Do not export jagen_install_dir

--- lib/env.sh
+++ lib/env.sh
@@ -45,7 +45,7 @@ jagen_target_prefix="/usr"
 jagen_tools_dir="$jagen_root/tools"
 jagen_tools_prefix=""
 
-export jagen_install_dir="$jagen_root/firmware"
+jagen_install_dir="$jagen_root/firmware"
 
 add_PATH "$jagen_host_dir/bin"
 add_LD_LIBRARY_PATH "$jagen_host_dir/lib"
