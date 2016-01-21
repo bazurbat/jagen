b6af2807a8df48e25181396260784fe95c3559b5
Oleg Kolosov <bazurbat@gmail.com>
2016-01-21 12:45:30 +0300

    Use default_build in dbus pkg script

--- lib/pkg/dbus.lua
+++ lib/pkg/dbus.lua
@@ -3,6 +3,39 @@ package {
     source = 'dbus-1.6.18.tar.gz',
     build  = {
         type = 'GNU',
-        libs = { 'dbus-1' }
+        libs = { 'dbus-1' },
+        options = {
+            '--with-system-pid-file=/run/dbus.pid',
+            '--with-system-socket=/run/dbus/system_bus_socket',
+            '--disable-compiler-coverage',
+            '--disable-developer',
+            '--disable-ansi',
+            '--disable-verbose-mode',
+            '--disable-asserts',
+            '--disable-checks',
+            '--disable-xml-docs',
+            '--disable-doxygen-docs',
+            '--disable-abstract-sockets',
+            '--disable-selinux',
+            '--disable-libaudit',
+            '--disable-dnotify',
+            '--enable-inotify',
+            '--disable-kqueue',
+            '--disable-console-owner-file',
+            '--disable-userdb-cache',
+            '--disable-launchd',
+            '--disable-systemd',
+            '--disable-embedded-tests',
+            '--disable-modular-tests',
+            '--disable-tests',
+            '--disable-installed-tests',
+            '--disable-epoll',
+            '--disable-x11-autolaunch',
+            '--disable-Werror',
+            '--disable-stats',
+            '--with-xml=expat',
+            '--without-valgrind',
+            '--without-x'
+        }
     }
 }
--- lib/pkg/dbus.sh
+++ lib/pkg/dbus.sh
@@ -2,43 +2,8 @@
 
 jagen_pkg_build() {
     # configure fails to run expat test program without this
-    CFLAGS="$CFLAGS -I$pkg_dest_dir$pkg_prefix/include"
-    LDFLAGS="$LDFLAGS -L$pkg_dest_dir$pkg_prefix/lib"
+    CFLAGS="$CFLAGS -I$pkg_install_dir/include"
+    LDFLAGS="$LDFLAGS -L$pkg_install_dir/lib"
 
-    pkg_run "$pkg_source_dir/configure" \
-        --host="$pkg_system" \
-        --prefix="$pkg_prefix" \
-        --with-system-pid-file=/run/dbus.pid \
-        --with-system-socket=/run/dbus/system_bus_socket \
-        --disable-compiler-coverage \
-        --disable-developer \
-        --disable-ansi \
-        --disable-verbose-mode \
-        --disable-asserts \
-        --disable-checks \
-        --disable-xml-docs \
-        --disable-doxygen-docs \
-        --disable-abstract-sockets \
-        --disable-selinux \
-        --disable-libaudit \
-        --disable-dnotify \
-        --enable-inotify \
-        --disable-kqueue \
-        --disable-console-owner-file \
-        --disable-userdb-cache \
-        --disable-launchd \
-        --disable-systemd \
-        --disable-embedded-tests \
-        --disable-modular-tests \
-        --disable-tests \
-        --disable-installed-tests \
-        --disable-epoll \
-        --disable-x11-autolaunch \
-        --disable-Werror \
-        --disable-stats \
-        --with-xml=expat \
-        --without-valgrind \
-        --without-x
-
-    pkg_run make
+    default_build
 }
