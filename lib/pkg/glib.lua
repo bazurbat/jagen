462a8cb2f18f63eab3922e39822ddec905e1ab0b
Oleg Kolosov <bazurbat@gmail.com>
2016-01-21 14:32:22 +0300

    Clean up glib pkg scripts

--- lib/pkg/glib.lua
+++ lib/pkg/glib.lua
@@ -6,6 +6,21 @@ package {
     },
     build = {
         type = 'GNU',
+        options = {
+            '--disable-mem-pools',
+            '--disable-rebuilds',
+            '--disable-selinux',
+            '--disable-fam',
+            '--disable-xattr',
+            '--disable-libelf',
+            '--disable-gtk-doc-html',
+            '--disable-man',
+            '--disable-dtrace',
+            '--disable-systemtap',
+            '--disable-coverage',
+            '--disable-Bsymbolic',
+            '--disable-znodelete'
+        },
         libs = { 'glib-2.0', 'gthread-2.0', 'gobject-2.0', 'gmodule-2.0', 'gio-2.0' },
         autoreconf = true
     }
--- lib/pkg/glib.sh
+++ lib/pkg/glib.sh
@@ -8,30 +8,14 @@ jagen_pkg_patch() {
         glib/Makefile.am glib/Makefile.in
 }
 
-jagen_pkg_build() {
-    local cache="cross_cache.conf"
-    echo "glib_cv_stack_grows=no" > "$cache"
-    echo "ac_cv_func_posix_getpwuid_r=yes" >> "$cache"
-    echo "ac_cv_func_posix_getgrgid_r=yes" >> "$cache"
-    echo "glib_cv_uscore=yes" >> "$cache"
+jagen_pkg_build_target() {
+    local cache="config.cache"
+    cat >$cache <<'EOF'
+glib_cv_stack_grows=no
+glib_cv_uscore=yes
+ac_cv_func_posix_getpwuid_r=yes
+ac_cv_func_posix_getgrgid_r=yes
+EOF
 
-    pkg_run "$pkg_source_dir/configure" \
-        --cache-file="$cache" \
-        --host="$pkg_system" \
-        --prefix="$pkg_prefix" \
-        --disable-mem-pools \
-        --disable-rebuilds \
-        --disable-selinux \
-        --disable-fam \
-        --disable-xattr \
-        --disable-libelf \
-        --disable-gtk-doc-html \
-        --disable-man \
-        --disable-dtrace \
-        --disable-systemtap \
-        --disable-coverage \
-        --disable-Bsymbolic \
-        --disable-znodelete
-
-    pkg_run make
+    default_build --cache-file="$cache"
 }
