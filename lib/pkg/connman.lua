5c744c97107879a78e3496d43e1a59d4e0f77b32
Oleg Kolosov <bazurbat@gmail.com>
2016-01-21 13:22:43 +0300

    Remove redundant connman pkg script

--- lib/pkg/connman.lua
+++ lib/pkg/connman.lua
@@ -3,6 +3,19 @@ package {
     source = 'connman-1.28.tar.xz',
     build  = {
         type = 'GNU',
-        autoreconf = true
+        autoreconf = true,
+        options = {
+            '--sysconfdir=/etc',
+            '--localstatedir=/settings',
+            '--enable-pie',
+            '--disable-gadget',
+            '--disable-bluetooth',
+            '--disable-ofono',
+            '--disable-dundee',
+            '--disable-pacrunner',
+            '--disable-neard',
+            '--disable-wispr',
+            '--disable-client'
+        }
     }
 }
