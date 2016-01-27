5be1407b6444087f41f66fd0409547231aae4afb
Oleg Kolosov <bazurbat@gmail.com>
2016-01-27 14:26:55 +0300

    Fix pkg-config target env DIR override, allow system paths

--- lib/env/target.sh
+++ lib/env/target.sh
@@ -14,7 +14,14 @@ fi
 
 export PKG_CONFIG_SYSROOT_DIR="${jagen_target_dir}"
 export PKG_CONFIG_LIBDIR="${jagen_target_dir}${jagen_target_prefix}/lib/pkgconfig"
-export PKG_CONFIG_DIR=""
+export PKG_CONFIG_PATH=""
+
+# pkg-config tries to be smart and removes -I and -L flags from it's output
+# when they resemble system paths. This causes SYSROOT_DIR to not be added to
+# them, which prevents packages to find each other when building the sysroot
+# itself.
+export PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1
+export PKG_CONFIG_ALLOW_SYSTEM_LIBS=1
 
 export ARCH="$jagen_target_arch"
 export CROSS_COMPILE="$jagen_toolchain_prefix"
