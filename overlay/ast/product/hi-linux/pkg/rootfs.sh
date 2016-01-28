4fb37300586bd945850e9c5c60256cef0d1a36e1
Oleg Kolosov <bazurbat@gmail.com>
2016-01-28 18:50:34 +0300

    Fix hi-linux rootfs script to actually copy libs

--- overlay/ast/product/hi-linux/pkg/rootfs.sh
+++ overlay/ast/product/hi-linux/pkg/rootfs.sh
@@ -2,7 +2,7 @@
 
 jagen_pkg_install() {
     local src="$pkg_build_dir"
-    local dst="$pkg_source_dir"
+    local dst="$pkg_source_dir/usr"
 
     local programs="csi smplayer"
 
@@ -10,7 +10,8 @@ jagen_pkg_install() {
         pkg_run install -m 755 "$src/bin/$name" "$dst/bin"
     done
 
-    pkg_run rsync -tl --exclude='*' --include='*.so*' "$src/lib" "$dst"
+    pkg_run rsync -rtlm --include='*/' --include='*.so*' --exclude='*' \
+        "$src/lib" "$dst"
     pkg_run rsync -rtl "$src/lib/chicken" "$dst/lib"
     find "$dst/lib" -name '*.so*' -exec chmod 755 '{}' \+ || die
 }
