cf2a08f14187260956c96b97c7c874c28c10caa3
Oleg Kolosov <bazurbat@gmail.com>
2016-02-03 17:37:13 +0300

    Do not install dropbearkey in hi-linux rootfs

--- overlay/ast/product/hi-linux/pkg/rootfs.sh
+++ overlay/ast/product/hi-linux/pkg/rootfs.sh
@@ -15,7 +15,7 @@ jagen_pkg_install() {
     local src="$pkg_build_dir"
     local dst="$pkg_source_dir/usr"
 
-    local programs="csi smplayer dropbearkey"
+    local programs="csi smplayer"
 
     for name in $programs; do
         pkg_run install -m 755 "$src/bin/$name" "$dst/bin"
