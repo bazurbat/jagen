ebb7b8dfbca126f5b39479eb21e6d439ad574534
Oleg Kolosov <bazurbat@gmail.com>
2016-02-16 20:11:10 +0300

    Add necessary dependencies to pango pkg

--- lib/pkg/pango.lua
+++ lib/pkg/pango.lua
@@ -1,15 +1,18 @@
 package {
     source = 'pango-1.36.8.tar.xz',
     build = {
-        type    = 'GNU',
+        type = 'GNU',
+        autoreconf = true,
         options = {
             '--disable-debug',
             '--disable-introspection',
+            '--disable-gtk-doc',
             '--disable-gtk-doc-html',
+            '--disable-gtk-doc-pdf',
             '--disable-explicit-deps',
             '--without-xft',
             '--with-cairo',
         }
     },
-    requires = { 'cairo' }
+    requires = { 'cairo', 'fontconfig', 'harfbuzz' }
 }
