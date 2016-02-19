4bab0591327c72dac89f9599ef588477b9d7b314
Oleg Kolosov <bazurbat@gmail.com>
2016-02-20 00:48:50 +0300

    Wrap arm-hisiv200-linux toolchain to use ccache transparently

--- overlay/toolchain/arm-hisiv200-linux/pkg/toolchain.sh
+++ overlay/toolchain/arm-hisiv200-linux/pkg/toolchain.sh
@@ -5,13 +5,19 @@ jagen_pkg_install_target() {
     : ${jagen_toolchain_dir:?}
     : ${jagen_toolchain_prefix:?}
 
-    local bin name
+    local bin name path
 
-    rm -fr "$jagen_toolchain_bin_dir"
-    mkdir -p "$jagen_toolchain_bin_dir"
+    rm -fr "$jagen_toolchain_bin_dir" || return
+    mkdir -p "$jagen_toolchain_bin_dir" || return
 
-    for bin in "$jagen_toolchain_dir"/bin/*; do
-        name="$(basename "$bin" | cut -d- -f5-)"
-        ln -fsr "$bin" "${jagen_toolchain_prefix}${name}"
+    cd "$jagen_toolchain_dir/bin" || return
+
+    for bin in *; do
+        name=$(basename "$bin" | cut -d- -f5-)
+        path="${jagen_toolchain_prefix}${name}"
+        cat >"$path" <<EOF || return
+exec \$jagen_ccache "\$jagen_toolchain_dir/bin/$bin" "\$@"
+EOF
+        chmod +x "$path" || return
     done
 }
