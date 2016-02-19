b40fdc03668c5ea2a6a4b598b6247ce7e5fddd28
Oleg Kolosov <bazurbat@gmail.com>
2016-02-20 00:47:45 +0300

    Export jagen_toolchain_dir for use by toolchain wrapper scripts

--- lib/env.sh
+++ lib/env.sh
@@ -21,6 +21,7 @@ export jagen_build_dir="$jagen_root/build"
 export jagen_include_dir="$jagen_root/include"
 export jagen_log_dir="$jagen_build_dir"
 
+export jagen_toolchain_dir
 export jagen_sdk_dir
 
 jagen_build_verbose="no"
