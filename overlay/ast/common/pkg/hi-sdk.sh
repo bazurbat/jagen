a31fbcc5b8fef52684e2cb2b30cfec0272d7b140
Oleg Kolosov <bazurbat@gmail.com>
2016-03-23 13:39:48 +0300

    Remove special unpack stage from hi-sdk

--- overlay/ast/common/pkg/hi-sdk.sh
+++ overlay/ast/common/pkg/hi-sdk.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-jagen_pkg_unpack() {
-    pkg_unpack
-
-    pkg_link \
-        "$jagen_src_dir/hi-kernel" \
-        "$pkg_source_dir/source/kernel/linux-3.4.y"
-
-    pkg_link \
-        "$jagen_src_dir/hi-sample" \
-        "$pkg_source_dir/sample"
-}
-
 jagen_pkg_patch() {
     if [ "$jagen_sdk" = "hi-linux" ]; then
         case $jagen_target_board in
