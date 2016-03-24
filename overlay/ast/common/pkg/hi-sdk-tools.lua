00ab871b5389ed683adc660899aab4f76d93c2ba
Oleg Kolosov <bazurbat@gmail.com>
2016-03-24 14:05:02 +0300

    Remove custom unpack stage from hi-sdk-tools

--- overlay/ast/common/pkg/hi-sdk-tools.lua
+++ overlay/ast/common/pkg/hi-sdk-tools.lua
@@ -3,8 +3,5 @@ package {
         type     = 'dist',
         location = 'hi-sdk-tools-v2014112714.tar.xz',
         path     = 'tools'
-    },
-    { 'unpack',
-        { 'hi-sdk', 'unpack' }
     }
 }
--- overlay/ast/common/pkg/hi-sdk-tools.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-jagen_pkg_unpack() {
-    pkg_unpack
-
-    pkg_link "$pkg_source_dir" \
-        "${jagen_sdk_src_dir:?}/tools"
-}
