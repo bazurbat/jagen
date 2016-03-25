c280f732d5d82c2631bd33d41bd8265e2347eaaa
Oleg Kolosov <bazurbat@gmail.com>
2016-03-25 10:38:31 +0300

    Fix quotes in default config templates

--- lib/init/ast2xx/env.sh
+++ lib/init/ast2xx/env.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-jagen_overlays='ast/board/ast2xx sdk/android toolchain/arm-linux-androideabi ast/common ast/product/android-firmware'
+jagen_overlays='ast/board/ast2xx sdk/android toolchain/arm-linux-androideabi \
+ast/common ast/product/android-firmware'
 jagen_toolchain_dir='$jagen_base_dir/toolchain/android-ndk-r10d'
 jagen_sdk_dir='$jagen_src_dir/android'
