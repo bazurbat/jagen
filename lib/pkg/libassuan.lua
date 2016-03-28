ac1d073a96cc9e69073612c07bbe8671b627cf3a
Oleg Kolosov <bazurbat@gmail.com>
2016-03-28 11:46:41 +0300

    Add with-libgpg-error-prefix to libassuan script

--- lib/pkg/libassuan.lua
+++ lib/pkg/libassuan.lua
@@ -2,6 +2,9 @@ package {
     source = 'libassuan-2.1.2.tar.bz2',
     build  = {
         type = 'GNU',
+        options = {
+            '--with-libgpg-error-prefix=$pkg_sysroot'
+        },
         libs = { 'assuan' }
     },
     requires = { 'libgpg-error' }
