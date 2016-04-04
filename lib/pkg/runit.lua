87543a66cc9311bf3d0e48bb3c1213065c0d7047
Oleg Kolosov <bazurbat@gmail.com>
2016-04-04 16:16:38 +0300

    Add runit package (compile only)

---
+++ lib/pkg/runit.lua
@@ -0,0 +1,11 @@
+package {
+    source = {
+        type = 'dist',
+        location = 'runit-2.1.2.tar.gz',
+        dir = 'admin/runit-2.1.2'
+    },
+    build = {
+        type = 'make',
+        in_source = true
+    }
+}
