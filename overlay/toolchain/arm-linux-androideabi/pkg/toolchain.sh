03b57fca08f840c16556922dee36e4b88be059a3
Oleg Kolosov <bazurbat@gmail.com>
2016-04-11 16:20:57 +0300

    Do not delete jagen_toolchain_bin_dir in android toolchain install script

--- overlay/toolchain/arm-linux-androideabi/pkg/toolchain.sh
+++ overlay/toolchain/arm-linux-androideabi/pkg/toolchain.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
 jagen_pkg_install_target() {
-    : ${jagen_toolchain_bin_dir:?}
+    : ${jagen_root:?}
     : ${jagen_toolchain_dir:?}
-    : ${jagen_target_dir:?}
     : ${jagen_target_platform:?}
     : ${jagen_target_toolchain:?}
 
-    rm -fr "$jagen_toolchain_bin_dir"
-    mkdir -p "$jagen_toolchain_bin_dir"
+    # TODO: look at the make-standalone-toolchain script to see if we need to
+    # delete install-dir beforehand
 
     bash "$jagen_toolchain_dir/build/tools/make-standalone-toolchain.sh" \
         --system="linux-x86_64" \
         --platform="$jagen_target_platform" \
         --toolchain="$jagen_target_toolchain" \
-        --install-dir="${jagen_root}/${jagen_target_toolchain}"
+        --install-dir="$jagen_root/$jagen_target_toolchain"
 }
