97a376b5933ac7b6f467258c201a10b635a65fb5
Oleg Kolosov <bazurbat@gmail.com>
2016-04-11 17:44:33 +0300

    sigma: do not add include and library paths from rootfs to every toolchain invocation

--- overlay/toolchain/mipsel-linux-gnu/pkg/toolchain.sh
+++ overlay/toolchain/mipsel-linux-gnu/pkg/toolchain.sh
@@ -2,43 +2,29 @@
 
 make_tool() {
     local name="$1" pre_opt="$2" post_opt="$3"
-    cat >"${jagen_toolchain_prefix}${name}" <<EOF
-#!/bin/sh
-exec $ccache $gcc_dir/mips-linux-gnu-${name} $pre_opt "\$@" $post_opt
+    local path="${jagen_bin_dir}/${jagen_target_system}-${name}"
+    cat >"$path" <<EOF || return
+exec \$jagen_ccache "\$jagen_toolchain_dir/bin/mips-linux-gnu-${name}" $pre_opt "\$@" $post_opt
 EOF
+    chmod +x "$path"
 }
 
 jagen_pkg_install_target() {
-    : ${jagen_toolchain_dir:?}
-
     local common_tools="addr2line ar c++filt elfedit gcov gdb gdbtui gprof nm \
 objcopy objdump ranlib readelf size sprite strings strip"
-    local inc_opt="-isystem \"$jagen_sdk_rootfs_prefix/include\""
-    local lib_opt="-L\"$jagen_sdk_rootfs_prefix/lib\""
-
-    local gcc_path="$jagen_toolchain_dir/bin/mips-linux-gnu-gcc"
-    local gcc_dir=$(dirname "${gcc_path}")
-    local ccache
-
-    rm -fr "$jagen_toolchain_bin_dir"
-    mkdir -p "$jagen_toolchain_bin_dir"
+    local inc_opt= # "-isystem \"\$jagen_sdk_rootfs_prefix/include\""
+    local lib_opt= # "-L\"\$jagen_sdk_rootfs_prefix/lib\""
 
     make_tool ld -EL
     make_tool as -EL "$inc_opt"
 
     make_tool cpp -EL "$inc_opt $lib_opt"
 
-    in_flags ccache && ccache='$jagen_ccache'
-
     for name in c++ g++ gcc; do
         make_tool $name -EL "$inc_opt $lib_opt"
     done
 
-    ccache=""
-
     for name in $common_tools; do
         make_tool $name
     done
-
-    chmod 755 "$jagen_toolchain_bin_dir"/* || return
 }
