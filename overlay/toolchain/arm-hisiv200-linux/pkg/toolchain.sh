e98a4b4caf3890e852faef31aa9cd4dd8f2548ef
Oleg Kolosov <bazurbat@gmail.com>
2016-04-11 17:31:50 +0300

    Remove references to jagen_toolchain_bin_dir in arm-hisiv200-linux toolchain script

--- overlay/toolchain/arm-hisiv200-linux/pkg/toolchain.sh
+++ overlay/toolchain/arm-hisiv200-linux/pkg/toolchain.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
 
 jagen_pkg_install_target() {
-    : ${jagen_toolchain_bin_dir:?}
     : ${jagen_toolchain_dir:?}
-    : ${jagen_toolchain_prefix:?}
 
     local bin name path
-
-    rm -fr "$jagen_toolchain_bin_dir" || return
-    mkdir -p "$jagen_toolchain_bin_dir" || return
 
     cd "$jagen_toolchain_dir/bin" || return
 
     for bin in *; do
         name=$(basename "$bin" | cut -d- -f5-)
-        path="${jagen_toolchain_prefix}${name}"
+        path="${jagen_bin_dir}/${jagen_target_system}-${name}"
         cat >"$path" <<EOF || return
 exec \$jagen_ccache "\$jagen_toolchain_dir/bin/$bin" "\$@"
 EOF
