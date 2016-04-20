36f39d63ffcbbe603a93d4a2ba45a8e717e9ff82
Oleg Kolosov <bazurbat@gmail.com>
2016-04-20 18:43:16 +0300

    Use more conservative default CFLAGS for hi-linux

--- usr/sdk/hi-linux/env/target.sh
+++ usr/sdk/hi-linux/env/target.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-export CFLAGS="-mcpu=cortex-a9 -mfpu=vfpv3-d16 -mfloat-abi=softfp -msoft-float \
--O2 -fno-strict-aliasing"
+# export CFLAGS="-mcpu=cortex-a9 -mfpu=vfpv3-d16 -mfloat-abi=softfp -msoft-float \
+# -O2 -fno-strict-aliasing"
+export CFLAGS="-O2 -fno-strict-aliasing"
 export CXXFLAGS="$CFLAGS"
 export ASMFLAGS="$CFLAGS"
 export LDFLAGS=""
