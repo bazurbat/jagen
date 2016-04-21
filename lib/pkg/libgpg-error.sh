0d55ca8ef33c627ab30f94c5ca2dd1d23153c7a4
Oleg Kolosov <bazurbat@gmail.com>
2016-04-21 17:44:13 +0300

    Fix gpg-error-config patching to handle non empty pkg_prefix

--- lib/pkg/libgpg-error.sh
+++ lib/pkg/libgpg-error.sh
@@ -4,7 +4,7 @@ jagen_pkg_install() {
     pkg_install || return
 
     if [ "$pkg_sysroot" ]; then
-        pkg_run sed -ri "s|^(prefix=)$|\1$pkg_sysroot|" \
+        pkg_run sed -ri "s|^(prefix=)$pkg_prefix$|\1$pkg_sysroot|" \
             "${pkg_install_dir:?}/bin/gpg-error-config"
     fi
 }
