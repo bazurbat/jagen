1af1edc3422de5e73646f455d2b017a28f521467
Oleg Kolosov <bazurbat@gmail.com>
2016-04-21 17:48:22 +0300

    Clean up sigma utils pkg script

--- usr/product/sigma-firmware/pkg/utils.sh
+++ usr/product/sigma-firmware/pkg/utils.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
 
 jagen_pkg_configure_host() {
-    pkg_run cmake -G"$jagen_cmake_generator" \
-        -DCMAKE_BUILD_TYPE="$(pkg_cmake_build_type)" \
-        -DCMAKE_INSTALL_PREFIX="$jagen_host_dir" \
-        -DUSE_LOOPAES=0 \
-        ${losetup:+"-DLOSETUP=$losetup"} \
-        "$pkg_source_dir"
+    pkg_configure \
+        -DUSE_LOOPAES=NO
 }
 
 jagen_pkg_configure_target() {
-    pkg_run cmake -G"$jagen_cmake_generator" \
-        -DCMAKE_BUILD_TYPE="$(pkg_cmake_build_type)" \
-        -DCMAKE_INSTALL_PREFIX="$jagen_sdk_rootfs_root" \
-        -DUSE_LOOPAES=1 \
-        "$pkg_source_dir"
+    pkg_configure \
+        -DUSE_LOOPAES=YES
 }
