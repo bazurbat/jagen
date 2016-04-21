45a41afa5c708189feee54f5908d5dad9c06f7c1
Oleg Kolosov <bazurbat@gmail.com>
2016-04-21 11:44:03 +0300

    Set default target board for products

--- usr/product/hi-linux/init.sh
+++ usr/product/hi-linux/init.sh
@@ -6,3 +6,5 @@ jagen_vendor="ast"
 
 jagen_init_toolchain_dir='$jagen_base_dir/toolchain/arm-hisiv200-linux'
 jagen_init_sdk_dir='$jagen_src_dir/hi-sdk'
+
+jagen_target_board="${jagen_target_board:-ast2xx}"
--- usr/product/sigma-firmware/init.sh
+++ usr/product/sigma-firmware/init.sh
@@ -6,3 +6,5 @@ jagen_vendor="ast"
 
 jagen_init_toolchain_dir='$jagen_base_dir/toolchain/mips-2012.03'
 jagen_init_sdk_dir='$jagen_src_dir/sigma-mrua'
+
+jagen_target_board="${jagen_target_board:-ast100}"
