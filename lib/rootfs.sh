9ebb4c2c504842f39054513a711ec989e5e15669
Oleg Kolosov <bazurbat@gmail.com>
2016-04-22 19:32:11 +0300

    hi-linux: made /var/run symlink to /run

--- lib/rootfs.sh
+++ lib/rootfs.sh
@@ -22,6 +22,8 @@ jagen_rootfs_init() {
     pkg_run chmod 0700 "$dir/root"
     pkg_run chmod 1777 "$dir/tmp"
 
+    pkg_run ln -snf /run "$dir/var"
+
     # backward compatibility with Sigma firmware
     pkg_run ln -snf /var "$dir/settings"
 }
