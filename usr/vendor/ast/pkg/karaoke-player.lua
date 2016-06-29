6e836ea9e7e0f91706f223292d77d1a4d1c11d35
Oleg Kolosov <bazurbat@gmail.com>
2016-06-29 17:57:49 +0300

    Add rules to install dbus configs to karaoke-player

--- usr/vendor/ast/pkg/karaoke-player.lua
+++ usr/vendor/ast/pkg/karaoke-player.lua
@@ -6,5 +6,15 @@ return {
     },
     build = {
         type = 'CMake'
+    },
+    install = {
+        dbus_system_configs = {
+            'source/agent-smith/org.artsystem.agent.conf',
+            'source/fpservice/org.artsystem.fpservice.conf',
+        },
+        dbus_system_services = {
+            'source/agent-smith/org.artsystem.agent.service',
+            'source/fpservice/org.artsystem.fpservice.service',
+        },
     }
 }
