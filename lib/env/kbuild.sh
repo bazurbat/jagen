c3a2325cdd8f0577001af6abd9ca9dd4f0713a3a
Oleg Kolosov <bazurbat@gmail.com>
2016-06-30 18:19:04 +0300

    Put comment about broken SDKs to kbuild env

--- lib/env/kbuild.sh
+++ lib/env/kbuild.sh
@@ -1,1 +1,5 @@
-export KBUILD_OUTPUT="${pkg_build_dir:?}"
+#!/bin/sh
+
+# Unfortunately vendor SDKs assume kernel sources placement with hardcoded
+# relative paths, no luck with out of tree builds so far.
+# export KBUILD_OUTPUT="${pkg_build_dir:?}"
