4016a41dec44be5f38b5aca1f0f2714afcffc75b
Oleg Kolosov <bazurbat@gmail.com>
2016-07-06 19:51:14 +0300

    hi-linux: set target CC and CXX explicitly

--- usr/sdk/hi-linux/env/target.sh
+++ usr/sdk/hi-linux/env/target.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-# export CFLAGS="-mcpu=cortex-a9 -mfpu=vfpv3-d16 -mfloat-abi=softfp -msoft-float \
-# -O2 -fno-strict-aliasing"
-export CFLAGS="-O2 -fno-strict-aliasing"
+export CC="arm-hisiv200-linux-gcc"
+export CXX="arm-hisiv200-linux-g++"
+
+export CFLAGS=""
 export CXXFLAGS="$CFLAGS"
 export ASMFLAGS="$CFLAGS"
 export LDFLAGS=""
