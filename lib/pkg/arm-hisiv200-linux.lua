ab63714c2a73fadae9d81431236baba2e63fba33
Oleg Kolosov <bazurbat@gmail.com>
2016-07-15 16:51:27 +0300

    Adjust pkg scripts for arm-hisiv200-linux

---
+++ lib/pkg/arm-hisiv200-linux.lua
@@ -0,0 +1,10 @@
+return {
+    source = {
+        type = 'dist',
+        location = 'arm-hisiv200-linux.tar.bz2',
+        sha256sum = 'aa10eeed8ec7c7ff794683ff9c3e4c36fb327798db7d4616355bf02d43da24ba',
+    },
+    build = {
+        in_source = true
+    }
+}
--- lib/pkg/arm-hisiv200-linux.sh
+++ lib/pkg/arm-hisiv200-linux.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 jagen_pkg_install_target() {
-    : ${jagen_toolchain_dir:?}
+    local name path src_name src_dir
 
-    local bin name path
+    src_dir="${pkg_source_dir:?}/bin"
 
-    cd "$jagen_toolchain_dir/bin" || return
+    cd "${src_dir:?}" || return
 
-    for bin in *; do
-        name=$(basename "$bin" | cut -d- -f5-)
-        path="${jagen_bin_dir}/${jagen_target_system}-${name}"
+    for src_name in *; do
+        name=$(printf "$src_name" | cut -d- -f5-)
+        path="${jagen_bin_dir:?}/arm-hisiv200-linux-${name}"
         cat >"$path" <<EOF || return
-exec \$jagen_ccache "\$jagen_toolchain_dir/bin/$bin" "\$@"
+exec \$jagen_ccache "$src_dir/$src_name" "\$@"
 EOF
         chmod +x "$path" || return
     done
