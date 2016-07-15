e8a0feb484cf5caa09070d1de8536703d43067ef
Oleg Kolosov <bazurbat@gmail.com>
2016-07-15 18:55:55 +0300

    Remove toolchain layer

--- usr/toolchain/arm-linux-androideabi/env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-jagen_target_arch="arm"
-jagen_target_system="arm-linux-androideabi"
-
-jagen_target_toolchain="${jagen_target_system}-4.6"
-jagen_toolchain_bin_dir="${jagen_root}/${jagen_target_toolchain}/bin"
