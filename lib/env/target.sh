0a4ab3cf35cc19ab55730db53f5baa06c421e7e5
Oleg Kolosov <bazurbat@gmail.com>
2016-07-15 23:46:52 +0300

    Remove jagen_toolchain_bind_dir from target env

--- lib/env/target.sh
+++ lib/env/target.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-if [ -z "$jagen_toolchain_bin_dir" ]; then
-    jagen_toolchain_bin_dir="${jagen_toolchain_dir:?}/bin"
-fi
-
 if [ -z "$jagen_toolchain_prefix" ]; then
-    jagen_toolchain_prefix="${jagen_toolchain_bin_dir}/${jagen_target_system:?}-"
+	jagen_toolchain_prefix="${jagen_bin_dir:?}/${jagen_target_system:?}-"
 fi
 
-if [ -d "$jagen_toolchain_bin_dir" ]; then
-    add_PATH "$jagen_toolchain_bin_dir"
-fi
-
-export PKG_CONFIG_SYSROOT_DIR="${jagen_target_dir}"
-export PKG_CONFIG_LIBDIR="${jagen_target_dir}/lib/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="${jagen_target_dir:?}"
+export PKG_CONFIG_LIBDIR="${jagen_target_dir:?}/lib/pkgconfig"
 export PKG_CONFIG_PATH=""
 
 # pkg-config tries to be smart and removes -I and -L flags from it's output
@@ -23,5 +15,5 @@ export PKG_CONFIG_PATH=""
 export PKG_CONFIG_ALLOW_SYSTEM_CFLAGS=1
 export PKG_CONFIG_ALLOW_SYSTEM_LIBS=1
 
-export ARCH="$jagen_target_arch"
-export CROSS_COMPILE="$jagen_toolchain_prefix"
+export ARCH="${jagen_target_arch:?}"
+export CROSS_COMPILE="${jagen_toolchain_prefix:?}"
