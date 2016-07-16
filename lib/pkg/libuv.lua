aba532d7314869b9c72744b06efb8095ebdd7e62
Oleg Kolosov <bazurbat@gmail.com>
2016-07-16 10:05:08 +0300

    Add dir override to libuv pkg

--- lib/pkg/libuv.lua
+++ lib/pkg/libuv.lua
@@ -2,7 +2,8 @@ return {
     source = {
         type      = 'dist',
         location  = 'http://dist.libuv.org/dist/v1.4.2/libuv-v1.4.2.tar.gz',
-        sha256sum = '1006c4055cb3fb34293bb716be127d396550a68ac0bdfc21a6631c8d7a49ce0b'
+        sha256sum = '1006c4055cb3fb34293bb716be127d396550a68ac0bdfc21a6631c8d7a49ce0b',
+        dir       = 'libuv-1.4.2'
     },
     build  = {
         type    = 'GNU',
