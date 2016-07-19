d70d01231dd179222069704e0eda36c8ad7f7328
Oleg Kolosov <bazurbat@gmail.com>
2016-07-19 23:21:33 +0300

    Add AS and CPP to target toolchain env

--- lib/env/target_toolchain.sh
+++ lib/env/target_toolchain.sh
@@ -3,6 +3,7 @@
 : ${jagen_target_system:?}
 
 export AR="${jagen_target_system}-ar"
+export AS="${jagen_target_system}-as"
 export CC="${jagen_target_system}-gcc"
 export CXX="${jagen_target_system}-g++"
-export STRIP="${jagen_target_system}-strip"
+export CPP="${jagen_target_system}-cpp"
