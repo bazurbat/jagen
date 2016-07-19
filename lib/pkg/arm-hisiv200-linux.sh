97f9ecb1e096b537cb5e06b6f7d1af9c30913a80
Oleg Kolosov <bazurbat@gmail.com>
2016-07-19 23:46:39 +0300

    Use toolchain_generate_wrappers in arm-hisiv200-linux install

--- lib/pkg/arm-hisiv200-linux.sh
+++ lib/pkg/arm-hisiv200-linux.sh
@@ -5,18 +5,8 @@ jagen_pkg_unpack() {
 }
 
 jagen_pkg_install_target() {
-    local name path src_name src_dir
-
-    src_dir="${pkg_source_dir:?}/bin"
-
-    cd "${src_dir:?}" || return
-
-    for src_name in *; do
-        name=$(printf "$src_name" | cut -d- -f5-)
-        path="${jagen_bin_dir:?}/${jagen_target_system}-${name}"
-        cat >"$path" <<EOF || return
-exec \$jagen_ccache "$src_dir/$src_name" "\$@"
-EOF
-        chmod +x "$path" || return
-    done
+    toolchain_generate_wrappers    \
+        "${jagen_bin_dir:?}"       \
+        "${pkg_source_dir:?}/bin"  \
+        "${jagen_target_system:?}"
 }
