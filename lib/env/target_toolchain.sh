875b7b8d4c8fb617cc165f4d210600ac04c7a6d1
Oleg Kolosov <bazurbat@gmail.com>
2016-07-20 16:34:47 +0300

    Add more utilities to target_toolchain

--- lib/env/target_toolchain.sh
+++ lib/env/target_toolchain.sh
@@ -5,5 +5,12 @@
 export AR="${jagen_target_system}-ar"
 export AS="${jagen_target_system}-as"
 export CC="${jagen_target_system}-gcc"
-export CXX="${jagen_target_system}-g++"
 export CPP="${jagen_target_system}-cpp"
+export CXX="${jagen_target_system}-g++"
+export LD="${jagen_target_system}-ld"
+export NM="${jagen_target_system}-nm"
+export OBJCOPY="${jagen_target_system}-objcopy"
+export OBJDUMP="${jagen_target_system}-objdump"
+export RANLIB="${jagen_target_system}-ranlib"
+export STRIP="${jagen_target_system}-strip"
+
