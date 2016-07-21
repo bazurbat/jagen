ca3c6462fe8c55e420196d566c64591e94a669c0
Oleg Kolosov <bazurbat@gmail.com>
2016-07-21 17:14:03 +0300

    Adjust default target PKG_CONFIG_PATH

--- lib/env/target.sh
+++ lib/env/target.sh
@@ -4,9 +4,11 @@ if [ -z "$jagen_toolchain_prefix" ]; then
 	jagen_toolchain_prefix="${jagen_bin_dir:?}/${jagen_target_system:?}-"
 fi
 
-export PKG_CONFIG_SYSROOT_DIR="${jagen_target_dir:?}"
-export PKG_CONFIG_LIBDIR="${jagen_target_dir:?}/lib/pkgconfig"
-export PKG_CONFIG_PATH=""
+: ${jagen_target_dir:?}
+
+export PKG_CONFIG_SYSROOT_DIR="$jagen_target_dir"
+export PKG_CONFIG_LIBDIR="$jagen_target_dir/lib/pkgconfig"
+export PKG_CONFIG_PATH="$jagen_target_dir/usr/lib/pkgconfig"
 
 # pkg-config tries to be smart and removes -I and -L flags from it's output
 # when they resemble system paths. This causes SYSROOT_DIR to not be added to
