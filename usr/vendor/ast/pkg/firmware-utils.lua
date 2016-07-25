7e3f64582849043426b4ef993b5074ccb3682629
Oleg Kolosov <bazurbat@gmail.com>
2016-07-25 11:39:43 +0300

    Add firmware-utils pkg

---
+++ usr/vendor/ast/pkg/firmware-utils.lua
@@ -0,0 +1,10 @@
+return {
+    source = {
+        type     = 'git',
+        location = 'git@bitbucket.org:art-system/firmware-utils.git',
+        branch   = 'master'
+    },
+    build = {
+        type = 'CMake'
+    }
+}
