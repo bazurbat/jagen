3186545f4f0df1a39ddcff806971a4f86f4071df
Oleg Kolosov <bazurbat@gmail.com>
2016-07-25 15:39:59 +0300

    Force target toolchain for hdparm

--- lib/pkg/hdparm.sh
+++ lib/pkg/hdparm.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 jagen_pkg_compile() {
-	pkg_run make STRIP=:
+    pkg_run make STRIP=:
+}
+
+jagen_pkg_compile_target() {
+    use_env target_toolchain
+    pkg_run make STRIP=:
 }
 
 jagen_pkg_install() {
-	pkg_run install -vm755 hdparm "$pkg_install_dir/sbin"
+    pkg_run install -vm755 hdparm "$pkg_install_dir/sbin"
 }
