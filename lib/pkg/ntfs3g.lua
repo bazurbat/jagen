a65fae96a928c1e294f9ff02b3c388c2a40c55a1
Oleg Kolosov <bazurbat@gmail.com>
2016-08-04 11:52:02 +0300

    Add ntfs3g pkg

---
+++ lib/pkg/ntfs3g.lua
@@ -0,0 +1,21 @@
+return {
+    source = {
+        type      = 'dist',
+        location  = 'https://tuxera.com/opensource/ntfs-3g_ntfsprogs-2016.2.22.tgz',
+        sha256sum = 'd7b72c05e4b3493e6095be789a760c9f5f2b141812d5b885f3190c98802f1ea0'
+    },
+    build  = {
+        type = 'GNU',
+        options = {
+			'--disable-shared',
+			'--disable-static',
+			'--enable-ldscript',
+			'--disable-ldconfig',
+			'--disable-library',
+			'--disable-posix-acls',
+			'--disable-ntfsprogs',
+			'--without-uuid',
+			'--without-hd',
+        }
+    }
+}
