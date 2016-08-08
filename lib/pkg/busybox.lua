2da8cd04cb8ed7bf6f159a9272a95799d00aa27c
Oleg Kolosov <bazurbat@gmail.com>
2016-08-08 15:13:57 +0300

    Specify full location for busybox

--- lib/pkg/busybox.lua
+++ lib/pkg/busybox.lua
@@ -1,5 +1,9 @@
 return {
-    source = 'busybox-1.24.2.tar.bz2',
+    source = {
+        type = 'dist',
+        location = 'https://busybox.net/downloads/busybox-1.24.2.tar.bz2',
+        sha1sum = '03e6cfc8ddb2f709f308719a9b9f4818bc0a28d0'
+    },
     build  = {
         type = 'make',
         in_source = true
