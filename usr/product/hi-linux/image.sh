ac6ad3a5f733fb0cc008549a5bd1fa1f54e324b5
Oleg Kolosov <bazurbat@gmail.com>
2016-08-09 12:59:50 +0300

    hi-linux: improve image fs size calculation, add status message

--- usr/product/hi-linux/image.sh
+++ usr/product/hi-linux/image.sh
@@ -12,23 +12,32 @@ calculate_size() {
     echo $1
 }
 
+# add padding for filesystem structures
+calculate_fs_size() {
+    local size="${1:?}" divisor=8
+    # integer ceil
+    local padding=$(( size / divisor + (size % divisor != 0) ))
+    echo $((size + padding))
+}
+
 on_exit() {
     sudo umount "$mount_path"
     rmdir "$mount_path"
 }
 
 create_image() {
-    local source_dir="$jagen_target_dir"
+    local source_dir="$jagen_build_dir/image"
     local out_file="$jagen_build_dir/rootfs.ext4"
-    local size="$(calculate_size "$source_dir")"
+    local size=$(calculate_size "$source_dir")
+    local fs_size=$(calculate_fs_size $size)
 
-    # add some space for filesystem structures
-    size=$((size + size/10))
     [ "$size" -gt 0 ] || die "Invalid image size, probably the $source_dir directory is empty"
 
-    dd if=/dev/zero of="$out_file" bs=1M count=0 seek="$size" 2>/dev/null
-    mke2fs -qF -t ext4 -O ^huge_file "$out_file" "${size}M"
- 
+    message "Creating $out_file (${size}M/${fs_size}M) from $source_dir"
+
+    dd if=/dev/zero of="$out_file" bs=1M count=0 seek="$fs_size" 2>/dev/null
+    mke2fs -qF -t ext4 -O ^huge_file "$out_file" "${fs_size}M"
+
     mount_path="$(mktemp -d)"
 
     sudo mount -t ext4 "$out_file" "$mount_path"
