5c9feb54f1f74ce1cc52230f7317af2091afc335
Oleg Kolosov <bazurbat@gmail.com>
2016-08-10 12:33:08 +0300

    Fix layer paths in init due to rename

--- usr/product/ast-hi-linux/init.sh
+++ usr/product/ast-hi-linux/init.sh
@@ -4,5 +4,5 @@ jagen_layers='
 $jagen_dir/usr/target/arm-hisiv200-linux
 $jagen_dir/usr/sdk/hi-linux
 $jagen_dir/usr/vendor/ast
-$jagen_dir/usr/product/hi-linux
+$jagen_dir/usr/product/ast-hi-linux
 '
--- usr/product/ast-sigma-firmware/init.sh
+++ usr/product/ast-sigma-firmware/init.sh
@@ -4,5 +4,5 @@ jagen_layers='
 $jagen_dir/usr/target/mipsel-linux-gnu
 $jagen_dir/usr/sdk/sigma
 $jagen_dir/usr/vendor/ast
-$jagen_dir/usr/product/sigma-firmware
+$jagen_dir/usr/product/ast-sigma-firmware
 '
