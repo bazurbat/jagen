c4bf35aa9bfd0b2f1de7afbf3c79e11abbb411d7
Oleg Kolosov <bazurbat@gmail.com>
2016-08-10 19:10:59 +0300

    Strip image on install

--- lib/pkg/image.sh
+++ lib/pkg/image.sh
@@ -5,3 +5,9 @@ pkg_source_dir="$pkg_install_dir"
 jagen_pkg_compile() {
     pkg_sync_dirs "$pkg_source_dir" "$pkg_build_dir" "image_contents.txt"
 }
+
+jagen_pkg_install() {
+    if pkg_is_release; then
+        pkg_strip_root "$pkg_build_dir"
+    fi
+}
