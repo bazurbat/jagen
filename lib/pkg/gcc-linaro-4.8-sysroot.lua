c42af4563de786d3390d58901b3efeb7d4aa6082
Oleg Kolosov <bazurbat@gmail.com>
2016-12-24 15:20:36 +0100

    Fix gcc-linaro-4.8 download location

--- lib/pkg/gcc-linaro-4.8-sysroot.lua
+++ lib/pkg/gcc-linaro-4.8-sysroot.lua
@@ -1,7 +1,7 @@
 return {
     source = {
         type = 'dist',
-        location = 'https://releases.linaro.org/15.06/components/toolchain/binaries/4.8/arm-linux-gnueabi/sysroot-linaro-glibc-gcc4.8-2015.06-arm-linux-gnueabi.tar.xz',
+        location = 'https://releases.linaro.org/archive/15.06/components/toolchain/binaries/4.8/arm-linux-gnueabi/sysroot-linaro-glibc-gcc4.8-2015.06-arm-linux-gnueabi.tar.xz',
         sha256sum = '45f9c9b31faa235b08d3339ba2598644ebcd753496893f4b24b24797f46b3d0b',
     },
     build = {
--- lib/pkg/gcc-linaro-4.8.lua
+++ lib/pkg/gcc-linaro-4.8.lua
@@ -1,7 +1,7 @@
 return {
     source = {
         type = 'dist',
-        location = 'https://releases.linaro.org/15.06/components/toolchain/binaries/4.8/arm-linux-gnueabi/gcc-linaro-4.8-2015.06-x86_64_arm-linux-gnueabi.tar.xz',
+        location = 'https://releases.linaro.org/archive/15.06/components/toolchain/binaries/4.8/arm-linux-gnueabi/gcc-linaro-4.8-2015.06-x86_64_arm-linux-gnueabi.tar.xz',
         sha256sum = '04556b1a453008222ab55e4ab9d792f32b6f8566e46e99384225a6d613851587',
     },
     build = {
