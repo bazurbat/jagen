b94c770377c710a8604b2bf44b752ee91aa4b6e9
Oleg Kolosov <bazurbat@gmail.com>
2017-01-19 11:16:19 +0100

    Patch capicxx-dbus-runtime patch to help it find DBus in different root

--- usr/vendor/genivi/pkg/capicxx-dbus-runtime.lua
+++ usr/vendor/genivi/pkg/capicxx-dbus-runtime.lua
@@ -3,6 +3,9 @@ return {
         type = 'git',
         location = 'https://github.com/GENIVI/capicxx-dbus-runtime.git'
     },
+    patches = {
+        { 'capicxx-dbus-runtime-Add-DBus-LIBDIR-to-link-directories', 1 }
+    },
     build = {
         type = 'CMake'
     },
