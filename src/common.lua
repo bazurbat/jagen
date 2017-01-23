911be409121a467bbe25758263b6efb2c93edf9f
Oleg Kolosov <bazurbat@gmail.com>
2017-01-23 23:57:59 +0100

    Add a couple of functional helpers

--- src/common.lua
+++ src/common.lua
@@ -7,6 +7,21 @@ local function assert_arg(fname, num, expected, value)
             num, fname, expected, got))
 end
 
+function apply_all(...)
+    local procs = {...}
+    return function (...)
+        for _, proc in ipairs(procs) do
+            proc(...)
+        end
+    end
+end
+
+function compose(f, g)
+    return function (...)
+        return f(g(...))
+    end
+end
+
 function append(ls, ...)
     assert_arg('append', 1, 'table', ls)
     for _, arg in ipairs({...}) do
@@ -98,12 +113,55 @@ function table.iextend(this, other)
     return this
 end
 
-function table.imap(arr, func)
+function table.map(t, func)
+    assert_arg('map', 1, 'table', t)
+    assert_arg('map', 2, 'function', func)
     local out = {}
-    for _, i in ipairs(arr) do
-        table.insert(out, func(i))
+    for _, v in pairs(t) do
+        table.insert(out, func(v))
     end
     return out
+end
+
+function table.imap(t, func)
+    assert_arg('imap', 1, 'table', t)
+    assert_arg('imap', 2, 'function', func)
+    local out = {}
+    for _, v in ipairs(t) do
+        table.insert(out, func(v))
+    end
+    return out
+end
+
+function table.for_each(t, func)
+    assert_arg('for_each', 1, 'table', t)
+    assert_arg('for_each', 2, 'function', func)
+    for _, v in pairs(t) do
+        func(v)
+    end
+end
+
+function table.filter(t, pred)
+    assert_arg('filter', 1, 'table', t)
+    assert_arg('filter', 2, 'function', pred)
+    local out = {}
+    for k, v in pairs(t) do
+        if pred(v) then
+            out[k] = v
+        end
+    end
+    return out
+end
+
+function table.find(t, pred)
+    assert_arg('find', 1, 'table', t)
+    assert_arg('find', 2, 'function', pred)
+    for k, v in pairs(t) do
+        if pred(v) then
+            return v, i
+        end
+    end
+    return nil, nil
 end
 
 function table.merge(to, from)
