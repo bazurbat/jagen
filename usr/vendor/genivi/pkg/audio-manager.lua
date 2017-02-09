ebca5c5acd6f176287b0f3e0ec9076f7b41f64e3
Oleg Kolosov <bazurbat@gmail.com>
2017-02-09 14:51:17 +0100

    Enable build of DBus wrapper for AudioManager

--- usr/vendor/genivi/pkg/audio-manager.lua
+++ usr/vendor/genivi/pkg/audio-manager.lua
@@ -6,7 +6,7 @@ return {
         ignore_dirty = true
     },
     patches = {
-        { 'audio-manager-pass-all-LDFLAGS-of-the-found-CommonAPI-to-the-build', 1 }
+        { 'audio-manager-pass-all-LDFLAGS-to-linker-when-building-wrappers', 1 }
     },
     build = {
         type = 'CMake',
@@ -18,7 +18,7 @@ return {
             '-DWITH_SYSTEMD_WATCHDOG=OFF',
             '-DGLIB_DBUS_TYPES_TOLERANT=ON',
             '-DWITH_CAPI_WRAPPER=ON',
-            '-DWITH_DBUS_WRAPPER=OFF',
+            '-DWITH_DBUS_WRAPPER=ON',
             '-DWITH_SHARED_UTILITIES=ON',
             '-DWITH_SHARED_CORE=ON',
         }
