0babc9b693afb3804dc91e21a8a2e8db0e864942
Oleg Kolosov <bazurbat@gmail.com>
2017-03-31 10:25:38 +0200

    Adjust capicxx-core-runtime pkg to use optional DLT patch

--- usr/vendor/genivi/pkg/capicxx-core-runtime.lua
+++ usr/vendor/genivi/pkg/capicxx-core-runtime.lua
@@ -3,6 +3,9 @@ return {
         type = 'git',
         location = 'https://github.com/GENIVI/capicxx-core-runtime.git'
     },
+    patches = {
+        { 'capicxx-core-runtime-Add-USE_DLT-option-to-CMakeLists', 1 }
+    },
     build = {
         type = 'CMake',
         options = {
