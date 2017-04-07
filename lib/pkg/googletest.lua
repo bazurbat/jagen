cfcaaeb9c011e1c7a97b928dadd1a080f9610f46
Oleg Kolosov <bazurbat@gmail.com>
2017-04-07 11:50:16 +0200

    Add googletest pkg

---
+++ lib/pkg/googletest.lua
@@ -0,0 +1,10 @@
+return {
+    source = {
+        type     = 'git',
+        location = 'https://github.com/google/googletest.git',
+        branch   = 'release-1.8.0'
+    },
+    build = {
+        type = 'CMake'
+	}
+}
