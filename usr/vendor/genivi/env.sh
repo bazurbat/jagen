cf57387ff0c4ed1f8d8149b9a189d7d2db341c3a
Oleg Kolosov <bazurbat@gmail.com>
2017-05-24 15:27:16 +0200

    Adjust default genivi env to work from host prefix

--- usr/vendor/genivi/env.sh
+++ usr/vendor/genivi/env.sh
@@ -2,3 +2,6 @@
 
 export COMMONAPI_CONFIG="$jagen_host_dir/etc/commonapi.ini"
 export COMMONAPI_DBUS_CONFIG="$jagen_host_dir/etc/commonapi-dbus.ini"
+
+export PERS_CLIENT_DBUS_ADDRESS="${DBUS_SESSION_BUS_ADDRESS-}"
+export PERS_CLIENT_LIB_CUSTOM_LOAD="$jagen_host_dir/etc/pclCustomLibConfigFile.cfg"
