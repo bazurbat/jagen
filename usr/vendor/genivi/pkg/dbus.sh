81adfbbdc03292da3bfbb7f5768dd1f2f149da6f
Oleg Kolosov <bazurbat@gmail.com>
2017-08-08 14:46:55 +0200

    Adjust genivi dbus options to allow running host system daemon from install dir

--- usr/vendor/genivi/pkg/dbus.sh
+++ usr/vendor/genivi/pkg/dbus.sh
@@ -13,3 +13,14 @@ jagen_pkg_patch() {
         pkg_run patch -p1 -i"$patchfile"
     done
 }
+
+jagen_pkg_configure_host() {
+    pkg_configure \
+        "--with-system-pid-file=$pkg_install_dir/run/dbus.pid" \
+        "--with-system-socket=$pkg_install_dir/run/dbus/system_bus_socket"
+}
+
+jagen_pkg_install_host() {
+    pkg_install
+    pkg_run mkdir -p "$pkg_install_dir/run/dbus"
+}
