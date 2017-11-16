6527f12f61e353c5ada0aba8c24202de522e26ae
Oleg Kolosov <bazurbat@gmail.com>
2017-11-16 17:10:54 +0100

    Dump expat version to 2.2.5

--- lib/pkg/expat.lua
+++ lib/pkg/expat.lua
@@ -1,8 +1,8 @@
 return {
     source = {
-        type     = 'dist',
-        location = 'http://downloads.sourceforge.net/project/expat/expat/2.1.0/expat-2.1.0.tar.gz',
-        sha1sum  = 'b08197d146930a5543a7b99e871cba3da614f6f0'
+        type      = 'dist',
+        location  = 'https://github.com/libexpat/libexpat/releases/download/R_2_2_5/expat-2.2.5.tar.bz2',
+        sha256sum = 'd9dc32efba7e74f788fcc4f212a43216fc37cf5f23f4c2339664d473353aedf6'
     },
     build  = {
         type = 'GNU',
