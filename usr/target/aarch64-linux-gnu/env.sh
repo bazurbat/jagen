abf8c6c1abbceec5d9a216d154b9af99a185be8e
Oleg Kolosov <bazurbat@gmail.com>
2017-11-17 14:47:43 +0100

    Remove jagen_target_cflags from aarch64 target

--- usr/target/aarch64-linux-gnu/env.sh
+++ usr/target/aarch64-linux-gnu/env.sh
@@ -5,5 +5,3 @@ jagen_target_system="${jagen_target_arch}-linux-gnu"
 
 jagen_target_toolchain="gcc-linaro-5.3_aarch64"
 jagen_target_toolchain_dir="${jagen_toolchains_dir:?}/gcc-linaro-5.3.1-2016.05-x86_64_aarch64-linux-gnu"
-
-# jagen_target_cflags="-march=armv7-a -mcpu=cortex-a9 -mfpu=vfpv3-d16 -mfloat-abi=softfp"
