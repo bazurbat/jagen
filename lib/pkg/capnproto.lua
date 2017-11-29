0b1288a15cc5af2df82c0a9923186fb6db291edd
Oleg Kolosov <bazurbat@gmail.com>
2017-11-29 16:13:16 +0100

    Disable unit tests for capnproto pkg to allow build on target

--- lib/pkg/capnproto.lua
+++ lib/pkg/capnproto.lua
@@ -4,6 +4,12 @@ return {
         location = 'https://capnproto.org/capnproto-c++-0.6.1.tar.gz'
     },
     build = {
-        type = 'CMake'
+        type = 'CMake',
+        options = {
+            -- Disable unit tests. Also allows independent build for target
+            -- because tests require running 'capnp_tool' executable on the
+            -- host.
+            '-DBUILD_TESTING=NO'
+        }
     }
 }
