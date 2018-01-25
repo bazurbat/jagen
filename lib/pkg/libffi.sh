afa1b518715938cf3a9a732f14393d7204000a96
Oleg Kolosov <bazurbat@gmail.com>
2018-01-25 14:23:43 +0100

    Remove custom configure from libffi

--- lib/pkg/libffi.sh
+++ lib/pkg/libffi.sh
@@ -6,13 +6,3 @@ jagen_pkg_patch() {
     # https://bugs.gentoo.org/462814
     pkg_run sed -i -e 's:@toolexeclibdir@:$(libdir):g' Makefile.in
 }
-
-jagen_pkg_configure() {
-    case $jagen_sdk in
-        sigma)
-            # Needed starting from GCC 4.4
-            CFLAGS="$CFLAGS -mno-compact-eh"
-            ;;
-    esac
-    pkg_configure
-}
---
+++ usr/sdk/sigma/pkg/libffi.lua
@@ -0,0 +1,18 @@
+return {
+    source = {
+        type      = 'dist',
+        location  = 'ftp://sourceware.org/pub/libffi/libffi-3.2.1.tar.gz',
+        md5sum    = '83b89587607e3eb65c70d361f13bab43',
+        sha512sum = 'fed5f6eec86144608966857f54bd69a5faa43427f27bc9178ebe2c7a1cecf925c20dbd6df07a207ae469842874efcf5b99fb7e09db59cbd92ebfc0a7e1bb'
+    },
+    build = {
+        type    = 'GNU',
+        options = { '--disable-builddir' },
+        libs    = { 'ffi' },
+        autoreconf = true
+    },
+    env = {
+        -- upstream default + -mno-compact-eh
+        CFLAGS = '-O3 -fomit-frame-pointer -fstrict-aliasing -ffast-math -mno-compact-eh'
+    }
+}
