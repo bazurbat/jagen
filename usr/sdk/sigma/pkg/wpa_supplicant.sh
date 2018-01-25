6c7939c95e32640be36994cbd03a4cf91ad2f209
Oleg Kolosov <bazurbat@gmail.com>
2018-01-25 15:21:59 +0100

    Make wpa_supplicant pkg to vendor and sdk specific

---
+++ usr/sdk/sigma/pkg/wpa_supplicant.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+jagen_pkg_configure_target() {
+    rm -f .config
+
+    echo "BINDIR=$pkg_install_prefix/bin"    >> .config
+
+    echo "CONFIG_CTRL_IFACE=y"               >> .config
+    echo "CONFIG_BACKEND=file"               >> .config
+
+    echo "CONFIG_PEERKEY=y"                  >> .config
+
+    echo "CONFIG_BGSCAN_SIMPLE=y"            >> .config
+    echo "CONFIG_BGSCAN_LEARN=y"             >> .config
+
+    echo "CONFIG_TLS=internal"               >> .config
+    echo "CONFIG_INTERNAL_LIBTOMMATH=y"      >> .config
+    echo "CONFIG_INTERNAL_LIBTOMMATH_FAST=y" >> .config
+
+    echo "CONFIG_CTRL_IFACE_DBUS_NEW=y"      >> .config
+
+    echo "CONFIG_DRIVER_WEXT=y"              >> .config
+
+    echo "CONFIG_DELAYED_MIC_ERROR_REPORT=y" >> .config
+
+    echo "CONFIG_IEEE8021X_EAPOL=y"          >> .config
+}
--- usr/vendor/ast/pkg/wpa_supplicant.sh
+++ usr/vendor/ast/pkg/wpa_supplicant.sh
@@ -1,41 +1,5 @@
 #!/bin/sh
 
-jagen_pkg_configure_target() {
-    rm -f .config
-
-    echo "BINDIR=$pkg_install_prefix/bin"    >> .config
-
-    echo "CONFIG_CTRL_IFACE=y"               >> .config
-    echo "CONFIG_BACKEND=file"               >> .config
-
-    echo "CONFIG_PEERKEY=y"                  >> .config
-
-    echo "CONFIG_BGSCAN_SIMPLE=y"            >> .config
-    echo "CONFIG_BGSCAN_LEARN=y"             >> .config
-
-    echo "CONFIG_TLS=internal"               >> .config
-    echo "CONFIG_INTERNAL_LIBTOMMATH=y"      >> .config
-    echo "CONFIG_INTERNAL_LIBTOMMATH_FAST=y" >> .config
-
-    echo "CONFIG_CTRL_IFACE_DBUS_NEW=y"      >> .config
-
-    case $jagen_sdk in
-        hi-linux)
-            echo "CFLAGS += $(pkg-config --cflags libnl-3.0)" >> .config
-            echo "LIBS   += $(pkg-config --libs libnl-3.0)"   >> .config
-            echo "CONFIG_DRIVER_NL80211=y"   >> .config
-            echo "CONFIG_LIBNL32=y"          >> .config
-            ;;
-        *)
-            echo "CONFIG_DRIVER_WEXT=y"      >> .config
-            ;;
-    esac
-
-    echo "CONFIG_DELAYED_MIC_ERROR_REPORT=y" >> .config
-
-    echo "CONFIG_IEEE8021X_EAPOL=y"          >> .config
-}
-
 jagen_pkg_install() {
     local conf_dir="$pkg_install_dir/etc"
 
