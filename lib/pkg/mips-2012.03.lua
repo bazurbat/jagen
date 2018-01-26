6c1287bf946b7bb248838e3889db3a98e90389e2
Oleg Kolosov <bazurbat@gmail.com>
2018-01-26 23:19:12 +0100

    Add download location for mips-2012.03 toolchain

--- lib/pkg/mips-2012.03.lua
+++ lib/pkg/mips-2012.03.lua
@@ -1,9 +1,11 @@
 return {
     source = {
-        type = 'dist',
-        location = 'mips-2012.03-63-mips-linux-gnu-i686-pc-linux-gnu.tar.bz2',
+        type      = 'dist',
+        -- https://sourcery.mentor.com/GNUToolchain/subscription3130?lite=MIPS
+        location  = 'https://sourcery.mentor.com/GNUToolchain/package10395/public/mips-linux-gnu/mips-2012.03-63-mips-linux-gnu-i686-pc-linux-gnu.tar.bz2',
         sha256sum = '0a2d92bbca2926479662affcc4dcb644ddca7d8d50ec17ed3b99d7025a552530',
-        dir = 'mips-2012.03'
+        md5sum    = '44970134eea6f6ab754a7bbe6e2805d6',
+        dir       = 'mips-2012.03'
     },
     build = {
         arch   = 'mips',
