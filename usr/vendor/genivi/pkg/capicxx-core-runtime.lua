c87798a6316073d2ac4570b818bd0bba9fd07ad1
Oleg Kolosov <bazurbat@gmail.com>
2018-02-01 18:45:10 +0100

    Remove DLT option patch from capicxx-core-runtime
    It's no longer needed.

--- usr/vendor/genivi/pkg/capicxx-core-runtime.lua
+++ usr/vendor/genivi/pkg/capicxx-core-runtime.lua
@@ -4,9 +4,6 @@ return {
         location = 'https://github.com/GENIVI/capicxx-core-runtime.git',
         branch   = '3.1.12.1'
     },
-    patches = {
-        { 'capicxx-core-runtime-Add-USE_DLT-option', 1 }
-    },
     build = {
         type = 'CMake',
         options = {
