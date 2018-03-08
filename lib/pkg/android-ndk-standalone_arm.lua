0cededa89c3dcd0e070cd21ffa8c8bfbbf98a5e3
Oleg Kolosov <bazurbat@gmail.com>
2018-03-08 20:16:46 +0100

    Export cmake options from android standalone ndk

--- lib/pkg/android-ndk-standalone_arm.lua
+++ lib/pkg/android-ndk-standalone_arm.lua
@@ -6,5 +6,12 @@ return {
     },
     install = {
         type = 'android_ndk_toolchain'
+    },
+	export = {
+		cmake_options = {
+			'-DCMAKE_TOOLCHAIN_FILE=${toolchain_source_dir}/build/cmake/android.toolchain.cmake',
+			'-DANDROID_ABI=armeabi-v7a',
+			'-DANDROID_PLATFORM=android-23'
+		}
     }
 }
