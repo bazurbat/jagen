f59b846fece565f369efe8175e2ed5aefd89ff6b
Oleg Kolosov <bazurbat@gmail.com>
2018-03-18 22:13:51 +0100

    Add back gdb to the core
    Having it ready is very useful but making a cross-target debuger is quite non-intuitive.

---
+++ lib/pkg/gdb.lua
@@ -0,0 +1,24 @@
+return {
+    source = {
+        type      = 'dist',
+        location  = 'http://ftp.gnu.org/gnu/gdb/gdb-7.9.tar.xz',
+        sha256sum = '9b315651a16528f7af8c7d8284699fb0c965df316cc7339bb0b7bae335848392'
+    },
+    build  = {
+        type = 'gnu',
+        options = {
+            '--build=x86_64-linux-gnu',
+            '--host=x86_64-linux-gnu',
+            '--target=${target_toolchain_system}',
+            '--disable-binutils',
+            '--disable-etc',
+            '--disable-gas',
+            '--disable-gold',
+            '--disable-gprof',
+            '--disable-gdbserver',
+            '--disable-readline',
+            '--with-system-readline',
+            '--with-system-zlib',
+        }
+    }
+}
---
+++ lib/pkg/gdb.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+jagen_pkg_configure() {
+    # Workaround from: https://sourceware.org/bugzilla/show_bug.cgi?id=18113#c1
+    cat > makeinfo <<'EOF' || return
+#!/bin/sh
+echo "makeinfo (GNU texinfo) 5.2"
+EOF
+    pkg_run chmod a+x makeinfo
+    pkg_configure MAKEINFO="$(realpath ./makeinfo)"
+}
