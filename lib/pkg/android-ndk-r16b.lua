f400357c66cb01d30de00b61599534580939ff2f
Oleg Kolosov <bazurbat@gmail.com>
2018-04-03 17:03:45 +0200

    Fid usage of dir instead of basename key in android-ndk pkg

--- lib/pkg/android-ndk-r16b.lua
+++ lib/pkg/android-ndk-r16b.lua
@@ -3,7 +3,7 @@ return {
         type     = 'dist',
         location = 'https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip',
         sha1sum  = '42aa43aae89a50d1c66c3f9fdecd676936da6128',
-        dir      = 'android-ndk-r16b'
+        basename = 'android-ndk-r16b'
     }
 }
 
