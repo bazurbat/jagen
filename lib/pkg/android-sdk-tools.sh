9aa612b4e3400ab30e3ebdd911a8f6994450db59
Oleg Kolosov <bazurbat@gmail.com>
2018-04-19 21:16:47 -0400

    Do not update the tools themselves each time on install android-sdk-tools

--- lib/pkg/android-sdk-tools.sh
+++ lib/pkg/android-sdk-tools.sh
@@ -4,5 +4,5 @@ jagen_pkg_install() {
     local sdkmanager="$pkg_source_dir/bin/sdkmanager"
     yes | "$sdkmanager" --licenses
     message "installing/updating tools using SDK Manager"
-    pkg_run "$sdkmanager" tools "cmake;3.6.4111459"
+    pkg_run "$sdkmanager" "cmake;3.6.4111459"
 }
