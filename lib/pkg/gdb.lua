5c149b57b0136d7aa6adc983e5261a317e158659
Oleg Kolosov <bazurbat@gmail.com>
2018-05-24 17:08:24 +0200

    Bump gdb version to 7.12.1

--- lib/pkg/gdb.lua
+++ lib/pkg/gdb.lua
@@ -1,8 +1,8 @@
 return {
     source = {
         type      = 'dist',
-        location  = 'http://ftp.gnu.org/gnu/gdb/gdb-7.9.tar.xz',
-        sha256sum = '9b315651a16528f7af8c7d8284699fb0c965df316cc7339bb0b7bae335848392'
+        location  = 'https://ftp.gnu.org/gnu/gdb/gdb-7.12.1.tar.xz',
+        sha256sum = '4607680b973d3ec92c30ad029f1b7dbde3876869e6b3a117d8a7e90081113186'
     },
     build  = {
         type = 'gnu',
