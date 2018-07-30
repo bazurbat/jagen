82cd05f5f7ab4ac5a8a7e6dbb63e0232f9e4917e
Oleg Kolosov <bazurbat@gmail.com>
2018-07-30 14:54:02 +0200

    Consider dirty source a fatal error on unpack stage
    But also honor ignore_dirty flag if set.

--- src/unpack.sh
+++ src/unpack.sh
@@ -105,8 +105,8 @@ pkg_unpack() {
             if [ -d "$pkg_source_dir" ] && ! jagen__is_empty "$pkg_source_dir"; then
                 if [ "$pkg_source_exclude" ]; then
                     message "not updating $pkg_name: the source is excluded"
-                elif jagen src dirty "$pkg_name"; then
-                    message "not updating $pkg_name: the source is dirty"
+                elif [ -z "$pkg_source_ignore_dirty" ] && jagen src dirty "$pkg_name"; then
+                    die "could not update $pkg_name: the source is dirty"
                 else
                     _jagen src clean "$pkg_name" || return
                     _jagen src update "$pkg_name" || return
