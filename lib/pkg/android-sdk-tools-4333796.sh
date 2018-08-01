4b8ff387cfb6321d630f46e8b740ab53ac0aa8e8
Oleg Kolosov <bazurbat@gmail.com>
2018-08-01 21:08:20 +0200

    Rename variant packages to have ~ suffix

--- lib/pkg/android-sdk-tools-4333796.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-jagen_pkg_install() {
-    local sdkmanager="$pkg_source_dir/bin/sdkmanager"
-    yes | "$sdkmanager" --licenses
-    message "installing/updating tools using SDK Manager"
-    pkg_run "$sdkmanager" "cmake;3.6.4111459"
-}
