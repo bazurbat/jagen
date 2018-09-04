7898cac8bc2fb4fa431cfaa7889dbe435db317f3
Oleg Kolosov <bazurbat@gmail.com>
2018-09-04 19:11:32 +0200

    Add libtool patches

--- lib/pkg/libtool.lua
+++ lib/pkg/libtool.lua
@@ -5,8 +5,8 @@ return {
         sha256sum = '65f940fbcb5f0727b10d759898afa35307b24c0945fb12f3f8387f5adb862ceb'
     },
     patches = {
-        { 'libtool-2.4.3-no-clean-gnulib',   1 },
-        { 'libtool-2.4.3-test-cmdline_wrap', 1 }
+        { 'libtool-2.4.3-no-clean-gnulib.patch',   1 },
+        { 'libtool-2.4.3-test-cmdline_wrap.patch', 1 }
     },
     build = {
         type = 'gnu',
