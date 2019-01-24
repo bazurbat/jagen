6337ab861735db02bc6703a0e45d08f246548d76
Oleg Kolosov <bazurbat@gmail.com>
2019-01-24 15:24:22 +0100

    Add gdbserver package

---
+++ lib/pkg/gdbserver.lua
@@ -0,0 +1,9 @@
+return {
+    source = {
+        type      = 'dist',
+        location  = 'https://ftp.gnu.org/gnu/gdb/gdb-7.12.1.tar.xz',
+        sha256sum = '4607680b973d3ec92c30ad029f1b7dbde3876869e6b3a117d8a7e90081113186',
+        dir       = 'gdb/gdbserver'
+    },
+    build = 'gnu'
+}
