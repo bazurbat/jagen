0b939d86211c9072c3f2d1f3c24daee8bd9d5f45
Oleg Kolosov <bazurbat@gmail.com>
2019-02-01 23:59:28 +0100

    Convert ignore_dirty to string in source status
    Lua 5.1 compatibility.

--- src/Jagen.lua
+++ src/Jagen.lua
@@ -126,7 +126,7 @@ function Jagen.source.status(args)
             local head = source:head_name()
             local dirty = source:dirty() and ' dirty' or ''
             if #dirty > 0 and source.ignore_dirty then
-                dirty = string.format(' dirty(ignored:%s)', source.ignore_dirty)
+                dirty = string.format(' dirty(ignored:%s)', tostring(source.ignore_dirty))
             end
             local exclude = source.exclude and ' excluded' or ''
             print(string.format("%s%s%s%s%s", pkg.name,
