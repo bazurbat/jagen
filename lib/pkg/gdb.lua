f5b453fbd6c2b1de4b01f18cd4a6338d3d2c4212
Oleg Kolosov <bazurbat@gmail.com>
2019-02-21 21:44:14 +0100

    Bump gdb to version 8.2

--- lib/pkg/gdb.lua
+++ lib/pkg/gdb.lua
@@ -1,21 +1,20 @@
 return {
     source = {
-        type      = 'dist',
-        location  = 'https://ftp.gnu.org/gnu/gdb/gdb-7.12.1.tar.xz',
-        sha256sum = '4607680b973d3ec92c30ad029f1b7dbde3876869e6b3a117d8a7e90081113186'
+        location  = 'https://ftp.gnu.org/gnu/gdb/gdb-8.2.tar.xz',
+        sha256sum = 'c3a441a29c7c89720b734e5a9c6289c0a06be7e0c76ef538f7bbcef389347c39'
     },
     build  = {
         type = 'gnu',
         options = {
             '--build=x86_64-linux-gnu',
             '--host=x86_64-linux-gnu',
-            '--target=${target_toolchain_system}',
             '--disable-binutils',
             '--disable-etc',
             '--disable-gas',
             '--disable-gold',
+            '--disable-ld',
             '--disable-gprof',
             '--disable-gdbserver'
         }
-    },
+    }
 }
