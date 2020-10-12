bazurbat/jagen

commit b05cfbee99cef34c10daf30f83b5b1421307c85a

Make options parsing distinguish long and short arguments

src/Options.lua
@@ -64,7 +64,7 @@ local function result_tostring(self)
         table.insert(lines, value)
     end
     for k, v in pairs(self) do
-        if v == true then
+        if type(v) == 'table' then
             append(string.format('--%s', k))
         end
     end
@@ -126,7 +126,10 @@ function P:parse(args)
                 return read_value(n+1, opt)
             end
         else
-            result[name] = true
+            if not result[name] then
+                result[name] = {}
+            end
+            result[name].long = true
         end
         return read_nth(n+1)
     end
@@ -153,7 +156,10 @@ function P:parse(args)
                 return read_value(n+1, opt)
             end
         else
-            result[opt.long] = true
+            if not result[opt.long] then
+                result[opt.long] = {}
+            end
+            result[opt.long].short = true
             return read_optstring(n, rest)
         end
     end

commit f6b3d4e7334d5f5f71e334a998037ca01b234086

Fix args stringification ignoring some flags
String conversion function of Options was incorrect causing it to miss
true/false values. In particular --quiet which is supposed to be set
implicitly for build command without arguments.

The regression introduced in b05cfbee.

src/Options.lua
@@ -64,7 +64,9 @@ local function result_tostring(self)
         table.insert(lines, value)
     end
     for k, v in pairs(self) do
-        if type(v) == 'table' then
+        if v == true or (type(v) == 'table' and
+                            (v.short == true or v.long == true))
+        then
             append(string.format('--%s', k))
         end
     end