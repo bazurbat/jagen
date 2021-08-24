c9aa0a357c7b82b13afce06592c9c7264fd20dac
Oleg Kolosov <jagen.dev@bazurbat.net>
2021-08-25 00:47:21 +0200

    Reimplement include Script generator to a new engine

--- src/Script.lua
+++ src/Script.lua
@@ -4,26 +4,39 @@ local Chunk  = require 'Chunk'
 
 local P = {}
 
-local function write_var(w, name, value)
-    local typ = type(value)
-    if typ == 'string' or typ == 'number' then
-        w("%s='%s'", name, value)
-    elseif typ == 'boolean' then
-        w("%s='yes'", name, value)
-    elseif typ == 'table' then
-        if #value > 0 then
-            local out = {}
-            for i, v in ipairs(value) do
-                if type(v) == 'table' then
-                else
-                    table.insert(out, v)
-                end
+local function is_simple_value(value)
+    local t = type(value)
+    return t == 'string' or t == 'number' or t == 'boolean'
+end
+
+local function write(fmt, name, value)
+    local tvalue = type(value)
+    name = string.to_identifier(name)
+    if tvalue == 'string' then
+        fmt("%s='%s'", name, value)
+    elseif tvalue == 'number' then
+        fmt("%s='%s'", name, tostring(value))
+    elseif tvalue == 'boolean' and value then
+        fmt("%s='true'", name)
+    elseif tvalue == 'table' then
+        local keys = table.keys(value)
+        table.sort(keys)
+        for key in each(keys) do
+            local newname = key
+            if #name > 0 then
+                newname = string.format('%s_%s', name, key)
             end
-            w("%s='%s'", name, table.concat(out, '\t'))
-        else
-            for k, v in pairs(value) do
-                write_var(w, name..'_'..k, v)
+            write(fmt, newname, value[key])
+        end
+        local array = {}
+        for i = 1, #value do
+            local item = value[i]
+            if is_simple_value(item) then
+                append(array, tostring(item))
             end
+        end
+        if #array > 0 then
+            fmt("%s='%s'", name, table.concat(array, '\t'))
         end
     end
 end
@@ -69,43 +82,74 @@ local function write_files(w, pkg)
 end
 
 function P:write(pkg, filename)
-    local lines = {}
+    local skip = { stages = true, uses = true, import = true, env = true }
 
-    local skip = { stages = true, import = true, uses = true }
-    local values = {}
-
-    local function collect(name, value, result)
-        if type(value) == 'table' then
-            for key, val in pairs(value) do
-                collect(name..'_'..key, val, result)
-            end
-        else
-            values[name] = value
+    local properties = {}
+    for key, val in pairs(pkg) do
+        if type(val) == 'string' then
+            append(properties, key)
+            skip[key] = true
         end
     end
+    table.sort(properties)
+
+    local standard_sections = {
+        source = true, build = true, install = true, export = true,
+        'source', 'build', 'install', 'export'
+    }
+    local other_sections = {}
 
     for key, val in pairs(pkg) do
-        if not skip[key] then
-            collect(key, val, values)
+        if not skip[key] and not standard_sections[key] then
+            append(other_sections, key)
+        end
+    end
+    table.sort(other_sections)
+
+    local lines, export_lines = {}, {}
+
+    local function add_line(s, ...)
+        append(lines, string.format(s, ...))
+    end
+    local function add_export_line(s, ...)
+        append(export_lines, string.format(s, ...))
+    end
+
+    for key in each(properties) do
+        write(add_line, key, pkg[key])
+    end
+
+    for name in each(standard_sections) do
+        if pkg[name] ~= nil then
+            write(add_line, name, pkg[name])
         end
     end
 
-    collect('import', pkg.import, values)
+    if pkg.import ~= nil then
+        write(add_line, 'import', pkg.import)
+    end
 
-    print(pretty(values))
+    for name in each(other_sections) do
+        write(add_line, name, pkg[name])
+    end
 
+    if pkg.env ~= nil then
+        write(add_line, 'pkg_env', pkg.env)
+        write(add_export_line, '', pkg.env)
+    end
 
-
-
-    -- write_section(pkg.import)
-    --
-    -- if pkg.env then
-    --     w('')
-    --     write_section(pkg.env, 'export ')
-    -- end
+    for i = 1, #lines do
+        lines[i] = string.format('pkg_%s', lines[i])
+    end
+    for i = 1, #export_lines do
+        export_lines[i] = string.format('export %s', export_lines[i])
+    end
 
     local file = assert(io.open(filename, 'w+'))
     file:write(table.concat(lines, '\n'), '\n')
+    if next(export_lines) then
+        file:write('\n', table.concat(export_lines, '\n'), '\n')
+    end
     file:close()
 end
 
